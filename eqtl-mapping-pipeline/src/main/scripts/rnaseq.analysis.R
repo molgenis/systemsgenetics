f897a7a14bbbebced075d328b941f62ffbdd3846
DannyArends <Danny.Arends@gmail.com>
2013-07-23 14:59:57 +0200

    Working RNA seq cell-type analysis

--- eqtl-mapping-pipeline/src/main/scripts/rnaseq.analysis.R
+++ eqtl-mapping-pipeline/src/main/scripts/rnaseq.analysis.R
@@ -15,7 +15,7 @@ RNASeq      <- read.csv("expression_table.genes.exonic_v69.0.3.rawCounts_Named.t
 # Affy neutrophils
 Neutr       <- read.csv("GPL570_Neutrophil_Good.txt", sep='\t', row.names=1)
 translation <- read.affy.translation()
-granulocytes
+
 Neutr <- annotate.affy.neutrohils(Neutr, translation)
 NeutrRNASeq <- match.annotated.affy.rnaseq(Neutr, RNASeq)
 
@@ -41,7 +41,7 @@ NeutrRNASeq <- NeutrRNASeq[inAffy, ]
 # Add the illumina data to the affy & RNA seq data
 RnaAffyIllu <- NULL
 for(gene in rownames(NeutrRNASeq)){
-  IlluMean <- mean(as.numeric(Illu[which(Illu[,1] == gene), 1]))
+  IlluMean <- mean(as.numeric(Illu[which(Illu[,1] == gene), 2]))
   AffyRowID <- which(rownames(NeutrRNASeq)==gene)
   RnaAffyIllu <- rbind(RnaAffyIllu, c(gene, IlluMean, NeutrRNASeq[AffyRowID,-8]))
 }
@@ -49,9 +49,13 @@ for(gene in rownames(NeutrRNASeq)){
 # Write the full table
 write.table(RnaAffyIllu,"NeutrophilIllumina_AllRNAseq_AllNeutroAffy_ByHUGO.txt", sep='\t', quote=FALSE)
 
-AffyMean  <- apply(RnaAffyIllu[,9:ncol(Neutr)], 1, mean)
-IlluMean  <- apply(RnaAffyIllu[,2], 1, mean)
+AffyMean  <- apply(RnaAffyIllu[,10:ncol(Neutr)], 1, function(x){mean(as.numeric(x))})
+IlluMean  <- as.numeric(RnaAffyIllu[,2])
+RNASeqLog  <- as.numeric(RnaAffyIllu[,"granulocytes"])
 
-CorAffyRNASeqMean <- round(cor(AffyMean, RNASeqLog, method="spearman"), d = 2)
+CorAffyRNASeqMean <- round(cor(AffyMean, as.numeric(RNASeqLog), method="spearman"), d = 2)
 plot(AffyMean, RNASeqLog, xlab = "Affy", ylab = "RNAseq", main=paste0("Mean Cor: ",CorAffyRNASeqMean), cex=0.7)
+
+CorIlluRNASeqMean <- round(cor(IlluMean, as.numeric(RNASeqLog), method="spearman"), d = 2)
+plot(IlluMean, RNASeqLog, xlab = "Illu", ylab = "RNAseq", main=paste0("Mean Cor: ",CorIlluRNASeqMean), cex=0.7)
 
