0b27195150a13d46a2e5aeaf320fbd640210fdf6
Patrick Deelen <patrickdeelen@gmail.com>
2013-07-25 22:18:44 +0200

    Untested genotype sample filter.

---
+++ molgenis-genotype-reader/src/main/java/org/molgenis/genotype/sampleFilter/ConvertToSampleFilterIterator.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.molgenis.genotype.sampleFilter;
+
+import java.util.Iterator;
+import org.molgenis.genotype.variant.GeneticVariant;
+
+/**
+ *
+ * @author Patrick Deelen
+ */
+public class ConvertToSampleFilterIterator implements Iterator<GeneticVariant> {
+
+	private final Iterator<GeneticVariant> originalIterator;
+	private final SampleFilterGenotypeData genotypeData;
+
+	public ConvertToSampleFilterIterator(Iterator<GeneticVariant> originalIterator, SampleFilterGenotypeData genotypeData) {
+		this.originalIterator = originalIterator;
+		this.genotypeData = genotypeData;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		return originalIterator.hasNext();
+	}
+
+	@Override
+	public GeneticVariant next() {
+		return new SampleFilteredReadOnlyGeneticVariant(originalIterator.next(), genotypeData);
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException("Not supported. Use modifiable genotype data to remove individual variants or use gc genotype data");
+	}
+
+	
+}
