5d79bfe467074051779c1321d60e1e9f4eb5575b
Patrick Deelen <patrickdeelen@gmail.com>
2013-08-03 13:14:15 +0200

    Better constructor for variant combined filter

--- molgenis-genotype-reader/src/main/java/org/molgenis/genotype/variantFilter/VariantCombinedFilter.java
+++ molgenis-genotype-reader/src/main/java/org/molgenis/genotype/variantFilter/VariantCombinedFilter.java
@@ -5,28 +5,44 @@
 package org.molgenis.genotype.variantFilter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.molgenis.genotype.variant.GeneticVariant;
 
 /**
  *
  * @author Patrick Deelen
  */
-public class VariantCombinedFilter implements VariantFilter{
-	
-	private final ArrayList<VariantFilter> variantFilters;
+public class VariantCombinedFilter implements VariantFilter {
 
-	public VariantCombinedFilter(ArrayList<VariantFilter> variantFilters) {
+	private final List<VariantFilter> variantFilters;
+
+	/**
+	 * Allows specification of arbitrary number of variant filters
+	 *
+	 * Example: new VariantCombinedFilter(new VariantQcChecker(0.05, 0.95,
+	 * 0.005), new VariantIdIncludeFilter(null));
+	 *
+	 * @param variantFilters
+	 */
+	public VariantCombinedFilter(VariantFilter... variantFilters) {
+		this.variantFilters = Arrays.asList(variantFilters);
+	}
+
+	public VariantCombinedFilter(List<VariantFilter> variantFilters) {
 		this.variantFilters = variantFilters == null ? new ArrayList<VariantFilter>() : variantFilters;
 	}
 
 	@Override
 	public boolean doesVariantPassFilter(GeneticVariant variant) {
-		for(VariantFilter filter : variantFilters){
-			if(! filter.doesVariantPassFilter(variant)){
+
+		for (VariantFilter filter : variantFilters) {
+			if (!filter.doesVariantPassFilter(variant)) {
 				return false;
 			}
 		}
 		return true;
+
+
 	}
-	
 }
