49da4193a068f339497152073230393324ca139c
Patrick Deelen <patrickdeelen@gmail.com>
2013-08-31 16:54:36 +0200

    Clean up of unused legacy code

--- molgenis-genotype-reader/src/main/java/org/molgenis/genotype/plink/datatypes/FamEntry.java
+++ molgenis-genotype-reader/src/main/java/org/molgenis/genotype/plink/datatypes/FamEntry.java
@@ -1,12 +1,8 @@
 package org.molgenis.genotype.plink.datatypes;
 
-import org.molgenis.util.tuple.KeyValueTuple;
-import org.molgenis.util.tuple.Tuple;
-import org.molgenis.util.tuple.WritableTuple;
-
-public class FamEntry
-{
+public class FamEntry {
 	// see: http://pngu.mgh.harvard.edu/~purcell/plink/data.shtml
+
 	String family;
 	String individual;
 	String father;
@@ -14,8 +10,7 @@ public class FamEntry
 	byte sex;
 	double phenotype;
 
-	public FamEntry(String family, String individual, String father, String mother, byte sex, double phenotype)
-	{
+	public FamEntry(String family, String individual, String father, String mother, byte sex, double phenotype) {
 		super();
 		this.family = family;
 		this.individual = individual;
@@ -25,52 +20,31 @@ public class FamEntry
 		this.phenotype = phenotype;
 	}
 
-	public static String[] famHeader()
-	{
-		return new String[]
-		{ "fam", "ind", "fa", "mo", "sex", "phen" };
+	public static String[] famHeader() {
+		return new String[]{"fam", "ind", "fa", "mo", "sex", "phen"};
 	}
 
-	public static Tuple famToTuple(FamEntry fam)
-	{
-		WritableTuple tuple = new KeyValueTuple();
-		tuple.set("fam", fam.getFamily());
-		tuple.set("ind", fam.getIndividual());
-		tuple.set("fa", fam.getFather());
-		tuple.set("mo", fam.getMother());
-		tuple.set("sex", fam.getSex());
-		tuple.set("phen", fam.getPhenotype());
-		return tuple;
-	}
-
-	public String getFamily()
-	{
+	public String getFamily() {
 		return family;
 	}
 
-	public String getIndividual()
-	{
+	public String getIndividual() {
 		return individual;
 	}
 
-	public String getFather()
-	{
+	public String getFather() {
 		return father;
 	}
 
-	public String getMother()
-	{
+	public String getMother() {
 		return mother;
 	}
 
-	public byte getSex()
-	{
+	public byte getSex() {
 		return sex;
 	}
 
-	public double getPhenotype()
-	{
+	public double getPhenotype() {
 		return phenotype;
 	}
-
 }
--- molgenis-genotype-reader/src/main/java/org/molgenis/genotype/plink/datatypes/PedEntry.java
+++ molgenis-genotype-reader/src/main/java/org/molgenis/genotype/plink/datatypes/PedEntry.java
@@ -3,59 +3,35 @@ package org.molgenis.genotype.plink.datatypes;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 import org.molgenis.genotype.Alleles;
-import org.molgenis.util.tuple.KeyValueTuple;
-import org.molgenis.util.tuple.Tuple;
-import org.molgenis.util.tuple.WritableTuple;
 
-public class PedEntry extends FamEntry implements Iterable<Alleles>
-{
+public class PedEntry extends FamEntry implements Iterable<Alleles> {
 
 	// list iterates SNP's, so 1 list per individual
 	private final Iterator<Alleles> bialleles;
 
 	public PedEntry(String family, String individual, String father, String mother, byte sex, double phenotype,
-			Iterator<Alleles> bialleles)
-	{
+			Iterator<Alleles> bialleles) {
 		super(family, individual, father, mother, sex, phenotype);
 		this.bialleles = bialleles;
 	}
 
 	@Override
-	public Iterator<Alleles> iterator()
-	{
+	public Iterator<Alleles> iterator() {
 		return bialleles;
 	}
 
-	public List<Alleles> getBialleles()
-	{
+	public List<Alleles> getBialleles() {
 		List<Alleles> bialleleList = new ArrayList<Alleles>();
 		Iterator<Alleles> it = iterator();
-		while (it.hasNext())
-		{
+		while (it.hasNext()) {
 			bialleleList.add(it.next());
 		}
 
 		return bialleleList;
 	}
 
-	public static String[] pedHeader()
-	{
-		return new String[]
-		{ "fam", "ind", "fa", "mo", "sex", "phen", "bial" };
-	}
-
-	public static Tuple pedToTuple(PedEntry ped)
-	{
-		WritableTuple tuple = new KeyValueTuple();
-		tuple.set("fam", ped.getFamily());
-		tuple.set("ind", ped.getIndividual());
-		tuple.set("fa", ped.getFather());
-		tuple.set("mo", ped.getMother());
-		tuple.set("sex", ped.getSex());
-		tuple.set("phen", ped.getPhenotype());
-		tuple.set("bial", ped.getBialleles());
-		return tuple;
+	public static String[] pedHeader() {
+		return new String[]{"fam", "ind", "fa", "mo", "sex", "phen", "bial"};
 	}
 }
