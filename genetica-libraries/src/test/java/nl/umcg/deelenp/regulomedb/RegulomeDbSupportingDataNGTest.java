1a937d576709e3e0af7aadee660d2df0a01f9720
Marc Jan Bonder <bonder.m.j@gmail.com>
2013-09-04 13:02:58 +0200

    Revert "Also renamed tests"
    This reverts commit b767280483e5a3da365c0f581e382cc72d8dcf03.

--- genetica-libraries/src/test/java/nl/umcg/deelenp/regulomedb/RegulomeDbSupportingDataNGTest.java
+++ genetica-libraries/src/test/java/nl/umcg/deelenp/regulomedb/RegulomeDbSupportingDataNGTest.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package umcg.genetica.io.regulomedb;
+package nl.umcg.deelenp.regulomedb;
 
 import umcg.genetica.io.regulomedb.RegulomeDbSupportingData;
 import static org.testng.Assert.*;
