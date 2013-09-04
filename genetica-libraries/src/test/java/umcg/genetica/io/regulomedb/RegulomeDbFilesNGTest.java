b767280483e5a3da365c0f581e382cc72d8dcf03
Marc Jan Bonder <bonder.m.j@gmail.com>
2013-09-04 13:02:22 +0200

    Also renamed tests

--- genetica-libraries/src/test/java/umcg/genetica/io/regulomedb/RegulomeDbFilesNGTest.java
+++ genetica-libraries/src/test/java/umcg/genetica/io/regulomedb/RegulomeDbFilesNGTest.java
@@ -2,11 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package nl.umcg.deelenp.regulomedb;
+package umcg.genetica.io.regulomedb;
 
 import umcg.genetica.io.regulomedb.RegulomeDbEntry;
 import umcg.genetica.io.regulomedb.RegulomeDbFile;
+import umcg.genetica.io.regulomedb.RegulomeDbFiles;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Iterator;
 import static org.testng.Assert.*;
 import org.testng.annotations.BeforeMethod;
@@ -16,21 +18,24 @@ import org.testng.annotations.Test;
  *
  * @author Patrick Deelen
  */
-public class RegulomeDbFileNGTest {
+public class RegulomeDbFilesNGTest {
 	
-	RegulomeDbFile instance;
+	RegulomeDbFiles instance;
 	
-	public RegulomeDbFileNGTest() {
+	public RegulomeDbFilesNGTest() {
 	}
 
 	@BeforeMethod
 	public void setUpMethod() throws Exception {
-		File regulomeDbFile = new File(this.getClass().getResource("/regulomeDbTestFile.txt").toURI());
-		instance = new RegulomeDbFile(regulomeDbFile);
+		ArrayList<File> files = new ArrayList<File>();
+		files.add(new File(this.getClass().getResource("/regulomeDbTestFile.txt").toURI()));
+		files.add(new File(this.getClass().getResource("/regulomeDbTestFileEmpty.txt").toURI()));
+		instance = new RegulomeDbFiles(files);
+		instance.addRegulomeDbFile(new RegulomeDbFile(new File(this.getClass().getResource("/regulomeDbTestFile2.txt").toURI())));
 	}
 
 	/**
-	 * Test of iterator method, of class RegulomeDbFile.
+	 * Test of iterator method, of class RegulomeDbFiles.
 	 */
 	@Test
 	public void testIterator() throws Exception {
@@ -58,6 +63,10 @@ public class RegulomeDbFileNGTest {
 		
 		currentEntry = regulomeDbFileIterator.next();
 		expectedEntry = new RegulomeDbEntry("chr1	752566	rs3094315	Single_Nucleotides|eQTL|FLJ22639, Motifs|Footprinting|NF-kappaB, Motifs|Footprinting|NFKB1, Motifs|Footprinting|, Chromatin_Structure|DNase-seq|Ag09319, Chromatin_Structure|DNase-seq|Hipe, Chromatin_Structure|DNase-seq|Hmveclly, Chromatin_Structure|DNase-seq|Hconf, Chromatin_Structure|DNase-seq|Hmvecdblneo, Chromatin_Structure|DNase-seq|Hmvecdlyad, Chromatin_Structure|DNase-seq|Hmvecdneo, Chromatin_Structure|DNase-seq|Huvec, Chromatin_Structure|DNase-seq|Nhdfneo, Chromatin_Structure|DNase-seq|Sknmc, Chromatin_Structure|DNase-seq|Nhdfad, Chromatin_Structure|DNase-seq|Hcm, Chromatin_Structure|DNase-seq|Lncap, Chromatin_Structure|DNase-seq|Hae, Chromatin_Structure|DNase-seq|Hcf, Chromatin_Structure|DNase-seq|Hmvecdlyneo, Chromatin_Structure|DNase-seq|Ag09309, Chromatin_Structure|DNase-seq|Hac, Chromatin_Structure|DNase-seq|Hrgec, Chromatin_Structure|DNase-seq|Hgf, Chromatin_Structure|DNase-seq|Hmec, Chromatin_Structure|DNase-seq|Hasp	1f");
+		assertEquals(currentEntry, expectedEntry);
+		
+		currentEntry = regulomeDbFileIterator.next();
+		expectedEntry = new RegulomeDbEntry("chr1	96560	rs3871782	Chromatin_Structure|DNase-seq|Ag09309	5");
 		assertEquals(currentEntry, expectedEntry);
 		
 		assertEquals(regulomeDbFileIterator.hasNext(), false);
