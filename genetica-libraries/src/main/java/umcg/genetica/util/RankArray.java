e43bbaf16b582922181481451d1eedc775c37441
Marc Jan Bonder <bonder.m.j@gmail.com>
2013-09-05 13:16:41 +0200

    Addition of fixing in colt ranking

--- genetica-libraries/src/main/java/umcg/genetica/util/RankArray.java
+++ genetica-libraries/src/main/java/umcg/genetica/util/RankArray.java
@@ -4,6 +4,10 @@
  */
 package umcg.genetica.util;
 
+import java.util.HashSet;
+import org.apache.commons.collections.primitives.ArrayDoubleList;
+import org.apache.commons.collections.primitives.ArrayFloatList;
+
 /**
  *
  * @author harmjan
@@ -87,54 +91,108 @@ public class RankArray {
     }
 
     public double[] rank(double[] x, boolean giveTiesSameRank) {
-        if (giveTiesSameRank) {
-            umcg.genetica.util.Rank rank = new umcg.genetica.util.Rank(x, 0d);
-            x = rank.getRanks();
-            for (int i = 0; i < x.length; i++) {
-                x[i] -= 1;
-            }
-            return x;
-
+        
+        this.xdouble = x.clone();
+        ydouble = new int[x.length];
+        for (int v = 0; v < x.length; v++) {
+            ydouble[v] = v;
+        }
+        cern.colt.GenericSorting.quickSort(0, x.length, compdouble, swapperdouble);
+        double[] rank = new double[x.length];
+        for (int v = 0; v < x.length; v++) {
+            rank[ydouble[v]] = v;
+        }
+        
+        if (!giveTiesSameRank) {
+            return rank;
         } else {
-            this.xdouble = x.clone();
-            ydouble = new int[x.length];
-            for (int v = 0; v < x.length; v++) {
-                ydouble[v] = v;
-            }
-            cern.colt.GenericSorting.quickSort(0, x.length, compdouble, swapperdouble);
-            double[] rank = new double[x.length];
-            for (int v = 0; v < x.length; v++) {
-                rank[ydouble[v]] = v;
-            }
+            fixTiesDouble(rank, x);
             return rank;
         }
     }
 
     public float[] rank(float[] x, boolean giveTiesSameRank) {
-        if (giveTiesSameRank) {
-
-            double[] xcopy = new double[x.length];
-            for (int i = 0; i < xcopy.length; i++) {
-                xcopy[i] = x[i];
-            }
-            umcg.genetica.util.Rank rank = new umcg.genetica.util.Rank(xcopy, 0d);
-            xcopy = rank.getRanks();
-            for (int i = 0; i < x.length; i++) {
-                x[i] = (float) xcopy[i] - 1;
-            }
-            return x;
-        } else {
-            this.x = x.clone();
-            y = new int[x.length];
-            for (int v = 0; v < x.length; v++) {
-                y[v] = v;
-            }
-            cern.colt.GenericSorting.quickSort(0, x.length, comp, swapper);
-            float[] rank = new float[x.length];
-            for (int v = 0; v < x.length; v++) {
-                rank[y[v]] = v;
-            }
+        
+        this.x = x.clone();
+        y = new int[x.length];
+        for (int v = 0; v < x.length; v++) {
+            y[v] = v;
+        }
+        cern.colt.GenericSorting.quickSort(0, x.length, comp, swapper);
+        float[] rank = new float[x.length];
+        for (int v = 0; v < x.length; v++) {
+            rank[y[v]] = v;
+        }
+        if (!giveTiesSameRank) {
             return rank;
+        } else {
+            fixTiesFloat(rank, x);
+            return rank;
+        }
+    }
+
+    private void fixTiesDouble(double[] rank, double[] x) {
+        HashSet<Double> fixedValues = new HashSet<Double>();
+        
+        for(int i=0; i<x.length;++i){
+            for(int j=0; j<x.length;++j){
+                if(x[i] == x[j] && !fixedValues.contains(x[i])){
+                    replaceRankDouble(x[i], rank, x);
+                    fixedValues.add(x[i]);
+                    break;
+                }
+            }
+        }
+        
+    }
+    
+    private void replaceRankDouble(double d, double[] rank, double[] x) {
+        ArrayDoubleList t = new ArrayDoubleList();
+        
+        for(int i = 0; i < x.length; i++){
+            if(x[i]==d){
+                t.add(rank[i]);
+            }
+        }
+        
+        double newRank = JSci.maths.ArrayMath.mean(t.toArray());
+        
+        for(int i = 0; i < x.length; i++){
+            if(x[i]==d){
+                rank[i] = newRank;
+            }
+        }
+    }
+    
+    private void fixTiesFloat(float[] rank, float[] x) {
+        HashSet<Float> fixedValues = new HashSet<Float>();
+        
+        for(int i=0; i<x.length;++i){
+            for(int j=0; j<x.length;++j){
+                if(x[i] == x[j] && !fixedValues.contains(x[i])){
+                    replaceRankFloat(x[i], rank, x);
+                    fixedValues.add(x[i]);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void replaceRankFloat(float f, float[] rank, float[] x) {
+        ArrayDoubleList t = new ArrayDoubleList();
+        
+        for(int i = 0; i < x.length; i++){
+            if(x[i]==f){
+                t.add(rank[i]);
+            }
+        }
+        
+        double newRank = JSci.maths.ArrayMath.mean(t.toArray());
+        
+        for(int i = 0; i < x.length; i++){
+            if(x[i]==f){
+                rank[i] = (float) newRank;
+            }
         }
     }
 }
---
+++ genetica-libraries/src/test/java/umcg/genetica/util/RankArrayNGTest.java
@@ -0,0 +1,94 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package umcg.genetica.util;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author MarcJan
+ */
+public class RankArrayNGTest {
+    
+    public RankArrayNGTest() {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    /**
+     * Test of rank method, of class RankArray.
+     */
+    @Test
+    public void testRank_doubleArr_boolean() {
+        double[] ranks = {4.0d, 2.0d, 0.0d, 7.0d, 5.0d, 8.0d, 9.0d, 1.0d, 6.0d, 3.0d};
+        double[] ranks1 = {4.0d, 5.0d, 2.0d, 0.0d, 8.0d, 6.0d, 9.0d, 10.0d, 1.0d, 7.0d, 3.0d};
+        
+        double[] ranksT = {5.0d, 2.0d, 0.0d, 7.0d, 5.0d, 8.0d, 9.0d, 1.0d, 5.0d, 3.0d};
+        double[] ranksT1 = {5.5d, 5.5d, 2.0d, 0.0d, 8.0d, 5.5d, 9.0d, 10.0d, 1.0d, 5.5d, 3.0d};
+        
+        double[] arrayOfDouble = {5.0d, 3.0d, 0.0d, 7.0d, 5.0d, 8.0d, 9.0d, 1.0d, 5.0d, 4.0d};
+        double[] arrayOfDouble1 = {5.0d, 5.0d, 3.0d, 0.0d, 7.0d, 5.0d, 8.0d, 9.0d, 1.0d, 5.0d, 4.0d};
+        
+        RankArray rda = new RankArray();
+            
+        double[] valsRanked = rda.rank(arrayOfDouble, false);
+        
+        boolean correct = true;
+        for(int i=0; i < ranks.length; ++i){
+            System.out.println(ranks[i]+"\t"+valsRanked[i]);
+            if(ranks[i]!=valsRanked[i]){
+                correct = false;
+                break;
+            }
+        }
+        assertEquals(correct, true);
+        
+        double[] valsRanked2 = rda.rank(arrayOfDouble1, false);
+        correct = true;
+        for(int i=0; i < ranks1.length; ++i){
+            System.out.println(ranks1[i]+"\t"+valsRanked2[i]);
+            if(ranks1[i]!=valsRanked2[i]){
+                correct = false;
+                break;
+            }
+        }
+        assertEquals(correct, true);
+        
+        double[] valsRankedT = rda.rank(arrayOfDouble, true);
+        
+        correct = true;
+        for(int i=0; i < ranks.length; ++i){
+            System.out.println(ranksT[i]+"\t"+valsRankedT[i]);
+            if(ranksT[i]!=valsRankedT[i]){
+                correct = false;
+//                break;
+            }
+        }
+        assertEquals(correct, true);
+        
+        double[] valsRankedT2 = rda.rank(arrayOfDouble1, true);
+        correct = true;
+        for(int i=0; i < ranks1.length; ++i){
+            System.out.println(ranksT1[i]+"\t"+valsRankedT2[i]);
+            if(ranksT1[i]!=valsRankedT2[i]){
+                correct = false;
+                break;
+            }
+        }
+        assertEquals(correct, true);
+        
+    }
+
+    /**
+     * Test of rank method, of class RankArray.
+     */
+    @Test
+    public void testRank_floatArr_boolean() {
+    }
+}
