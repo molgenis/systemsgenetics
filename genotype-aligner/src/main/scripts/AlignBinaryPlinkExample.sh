2c5811e72e0019eca6c10851a46c4a69e4845a59
Patrick Deelen <patrickdeelen@gmail.com>
2013-09-08 20:44:24 +0200

    Improved examples genotype aligner

--- genotype-aligner/src/main/scripts/AlignBinaryPlinkExample.sh
+++ genotype-aligner/src/main/scripts/AlignBinaryPlinkExample.sh
@@ -9,10 +9,10 @@ sh GenotypeAligner.sh \
 	--input ./exampleData/hapmap3CeuChr20B37Mb6RandomStrand \
 	--update-id \
 	--outputType PLINK_BED \
-	--output ./exampleOutput./binaryPlinkExampleOut \
+	--output ./exampleOutput/binaryPlinkExampleOut \
 	--refType VCF \
 	--ref ./exampleData/1000gCeuChr20Mb6
 
 
 echo ""
-echo "Demo output saved in: ${GenotypeAlignDir}/exampleOutput"
+echo "Demo output saved in: ${GenotypeAlignDir}/exampleOutput/"
