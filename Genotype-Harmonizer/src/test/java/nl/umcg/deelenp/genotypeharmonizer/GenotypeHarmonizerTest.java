194719b1b7ea8689914c006b967d02e668ee0461
Marc Jan Bonder <bonder.m.j@gmail.com>
2013-09-27 08:31:47 +0200

    Typo in package name

--- Genotype-Harmonizer/src/test/java/nl/umcg/deelenp/genotypeharmonizer/GenotypeHarmonizerTest.java
+++ Genotype-Harmonizer/src/test/java/nl/umcg/deelenp/genotypeharmonizer/GenotypeHarmonizerTest.java
@@ -2,9 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package nl.umcg.deelenp.genotypegarmonizer;
+package nl.umcg.deelenp.genotypeharmonizer;
 
-import nl.umcg.deelenp.genotypegarmonizer.GenotypeHarmonizer;
+import nl.umcg.deelenp.genotypeharmonizer.GenotypeHarmonizer;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
