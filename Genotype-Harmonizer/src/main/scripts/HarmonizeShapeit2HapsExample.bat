ecd307537b1bea76d319a746966f3478a7eebb71
Patrick Deelen <patrickdeelen@gmail.com>
2013-09-29 13:07:51 +0200

    Minor fix bat files genotype harmonizer

--- Genotype-Harmonizer/src/main/scripts/HarmonizeShapeit2HapsExample.bat
+++ Genotype-Harmonizer/src/main/scripts/HarmonizeShapeit2HapsExample.bat
@@ -4,5 +4,3 @@ cd /d %~dp0
 
 GenotypeHarmonizer.bat --inputType SHAPEIT2 --input .\exampleData\hapmap3CeuChr20B37Mb6RandomStrand --update-id --outputType SHAPEIT2 --output .\exampleOutput\shapeit2ExampleOut --refType VCF --ref .\exampleData\1000gCeuChr20Mb6 --forceChr 20
 
-echo ""
-echo "Demo output saved in: %genotypeHarmonizerDir%exampleOutput\"
