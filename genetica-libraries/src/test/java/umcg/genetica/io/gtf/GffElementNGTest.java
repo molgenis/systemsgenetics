b6b39d7b6670fa8709dc7de6f1ba900aacf3c0c8
Matthieu Beukers <matthieu.beukers@gmx.com>
2013-10-01 13:32:19 +0200

    Commented out the prototype tests for the GTF Reader so fails dont happen.

--- genetica-libraries/src/test/java/umcg/genetica/io/gtf/GffElementNGTest.java
+++ genetica-libraries/src/test/java/umcg/genetica/io/gtf/GffElementNGTest.java
@@ -1,195 +1,195 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package umcg.genetica.io.gtf;
-
-import java.util.Map;
-import static org.testng.Assert.*;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-/**
- *
- * @author Patrick Deelen
- */
-public class GffElementNGTest {
-	
-	public GffElementNGTest() {
-	}
-
-	@BeforeClass
-	public static void setUpClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownClass() throws Exception {
-	}
-
-	@BeforeMethod
-	public void setUpMethod() throws Exception {
-	}
-
-	@AfterMethod
-	public void tearDownMethod() throws Exception {
-	}
-
-	/**
-	 * Test of getSeqname method, of class GffElement.
-	 */
-	@Test
-	public void testGetSeqname() {
-		System.out.println("getSeqname");
-		GffElement instance = null;
-		String expResult = "";
-		String result = instance.getSeqname();
-		assertEquals(result, expResult);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getSource method, of class GffElement.
-	 */
-	@Test
-	public void testGetSource() {
-		System.out.println("getSource");
-		GffElement instance = null;
-		String expResult = "";
-		String result = instance.getSource();
-		assertEquals(result, expResult);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getFeature method, of class GffElement.
-	 */
-	@Test
-	public void testGetFeature() {
-		System.out.println("getFeature");
-		GffElement instance = null;
-		String expResult = "";
-		String result = instance.getFeature();
-		assertEquals(result, expResult);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getStart method, of class GffElement.
-	 */
-	@Test
-	public void testGetStart() {
-		System.out.println("getStart");
-		GffElement instance = null;
-		int expResult = 0;
-		int result = instance.getStart();
-		assertEquals(result, expResult);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getEnd method, of class GffElement.
-	 */
-	@Test
-	public void testGetEnd() {
-		System.out.println("getEnd");
-		GffElement instance = null;
-		int expResult = 0;
-		int result = instance.getEnd();
-		assertEquals(result, expResult);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getScore method, of class GffElement.
-	 */
-	@Test
-	public void testGetScore() {
-		System.out.println("getScore");
-		GffElement instance = null;
-		float expResult = 0.0F;
-		float result = instance.getScore();
-		assertEquals(result, expResult, 0.0);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getStrand method, of class GffElement.
-	 */
-	@Test
-	public void testGetStrand() {
-		System.out.println("getStrand");
-		GffElement instance = null;
-		char expResult = ' ';
-		char result = instance.getStrand();
-		assertEquals(result, expResult);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getFrame method, of class GffElement.
-	 */
-	@Test
-	public void testGetFrame() {
-		System.out.println("getFrame");
-		GffElement instance = null;
-		int expResult = 0;
-		int result = instance.getFrame();
-		assertEquals(result, expResult);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getAttributes method, of class GffElement.
-	 */
-	@Test
-	public void testGetAttributes() {
-		System.out.println("getAttributes");
-		GffElement instance = null;
-		Map expResult = null;
-		Map result = instance.getAttributes();
-		assertEquals(result, expResult);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of hasAttribute method, of class GffElement.
-	 */
-	@Test
-	public void testHasAttribute() {
-		System.out.println("hasAttribute");
-		String attributeName = "";
-		GffElement instance = null;
-		boolean expResult = false;
-		boolean result = instance.hasAttribute(attributeName);
-		assertEquals(result, expResult);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getAttributeValue method, of class GffElement.
-	 */
-	@Test
-	public void testGetAttributeValue() {
-		System.out.println("getAttributeValue");
-		String attributeName = "";
-		GffElement instance = null;
-		String expResult = "";
-		String result = instance.getAttributeValue(attributeName);
-		assertEquals(result, expResult);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-}
+///*
+// * To change this template, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package umcg.genetica.io.gtf;
+//
+//import java.util.Map;
+//import static org.testng.Assert.*;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Test;
+//
+///**
+// *
+// * @author Patrick Deelen
+// */
+//public class GffElementNGTest {
+//	
+//	public GffElementNGTest() {
+//	}
+//
+//	@BeforeClass
+//	public static void setUpClass() throws Exception {
+//	}
+//
+//	@AfterClass
+//	public static void tearDownClass() throws Exception {
+//	}
+//
+//	@BeforeMethod
+//	public void setUpMethod() throws Exception {
+//	}
+//
+//	@AfterMethod
+//	public void tearDownMethod() throws Exception {
+//	}
+//
+//	/**
+//	 * Test of getSeqname method, of class GffElement.
+//	 */
+//	@Test
+//	public void testGetSeqname() {
+//		System.out.println("getSeqname");
+//		GffElement instance = null;
+//		String expResult = "";
+//		String result = instance.getSeqname();
+//		assertEquals(result, expResult);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of getSource method, of class GffElement.
+//	 */
+//	@Test
+//	public void testGetSource() {
+//		System.out.println("getSource");
+//		GffElement instance = null;
+//		String expResult = "";
+//		String result = instance.getSource();
+//		assertEquals(result, expResult);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of getFeature method, of class GffElement.
+//	 */
+//	@Test
+//	public void testGetFeature() {
+//		System.out.println("getFeature");
+//		GffElement instance = null;
+//		String expResult = "";
+//		String result = instance.getFeature();
+//		assertEquals(result, expResult);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of getStart method, of class GffElement.
+//	 */
+//	@Test
+//	public void testGetStart() {
+//		System.out.println("getStart");
+//		GffElement instance = null;
+//		int expResult = 0;
+//		int result = instance.getStart();
+//		assertEquals(result, expResult);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of getEnd method, of class GffElement.
+//	 */
+//	@Test
+//	public void testGetEnd() {
+//		System.out.println("getEnd");
+//		GffElement instance = null;
+//		int expResult = 0;
+//		int result = instance.getEnd();
+//		assertEquals(result, expResult);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of getScore method, of class GffElement.
+//	 */
+//	@Test
+//	public void testGetScore() {
+//		System.out.println("getScore");
+//		GffElement instance = null;
+//		float expResult = 0.0F;
+//		float result = instance.getScore();
+//		assertEquals(result, expResult, 0.0);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of getStrand method, of class GffElement.
+//	 */
+//	@Test
+//	public void testGetStrand() {
+//		System.out.println("getStrand");
+//		GffElement instance = null;
+//		char expResult = ' ';
+//		char result = instance.getStrand();
+//		assertEquals(result, expResult);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of getFrame method, of class GffElement.
+//	 */
+//	@Test
+//	public void testGetFrame() {
+//		System.out.println("getFrame");
+//		GffElement instance = null;
+//		int expResult = 0;
+//		int result = instance.getFrame();
+//		assertEquals(result, expResult);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of getAttributes method, of class GffElement.
+//	 */
+//	@Test
+//	public void testGetAttributes() {
+//		System.out.println("getAttributes");
+//		GffElement instance = null;
+//		Map expResult = null;
+//		Map result = instance.getAttributes();
+//		assertEquals(result, expResult);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of hasAttribute method, of class GffElement.
+//	 */
+//	@Test
+//	public void testHasAttribute() {
+//		System.out.println("hasAttribute");
+//		String attributeName = "";
+//		GffElement instance = null;
+//		boolean expResult = false;
+//		boolean result = instance.hasAttribute(attributeName);
+//		assertEquals(result, expResult);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of getAttributeValue method, of class GffElement.
+//	 */
+//	@Test
+//	public void testGetAttributeValue() {
+//		System.out.println("getAttributeValue");
+//		String attributeName = "";
+//		GffElement instance = null;
+//		String expResult = "";
+//		String result = instance.getAttributeValue(attributeName);
+//		assertEquals(result, expResult);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//}
