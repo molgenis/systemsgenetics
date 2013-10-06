52d4969a8e5d8b88ffdbeb4f6013f365def559fe
Patrick Deelen <patrickdeelen@gmail.com>
2013-10-06 20:48:45 +0200

    fix imports

--- Genotype-IO/src/main/java/org/molgenis/genotype/RandomAccessGenotypeDataReaderFormats.java
+++ Genotype-IO/src/main/java/org/molgenis/genotype/RandomAccessGenotypeDataReaderFormats.java
@@ -3,7 +3,6 @@ package org.molgenis.genotype;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import org.apache.log4j.Logger;
 import org.molgenis.genotype.oxford.HapsGenotypeData;
 import org.molgenis.genotype.multipart.IncompatibleMultiPartGenotypeDataException;
 import org.molgenis.genotype.multipart.MultiPartGenotypeData;
