4a7605fd299b4c4195de6ae4e67072b7ae40ba72
Marc Jan Bonder <bonder.m.j@gmail.com>
2014-03-14 20:09:59 +0100

    Revert "Revert "Small optimizations""
    This reverts commit 31079c2deaac7a96df15224cefbb4a64edc3a573.

--- genetica-libraries/src/main/java/umcg/genetica/io/trityper/TriTyperExpressionData.java
+++ genetica-libraries/src/main/java/umcg/genetica/io/trityper/TriTyperExpressionData.java
@@ -562,10 +562,10 @@ public class TriTyperExpressionData {
         }
         
         //This is requered for the correlation matrix.
-        calcMeanAndVariance();
+        calcMeanAndVarianceOrg();
         for (int p = 0; p < matrix.length; p++) {
             for (int s = 0; s < matrix[p].length; s++) {
-                matrix[p][s] -= probeMean[p];
+                matrix[p][s] -= probeOriginalMean[p];
             }
         }
 
@@ -655,20 +655,29 @@ public class TriTyperExpressionData {
     private void calcMeanAndVariance() {
         //Precalculate means and variances. This will improve calculations substantially:
         int probeCount = probes.length;
-        probeOriginalMean = new double[probeCount];
-        probeOriginalVariance = new double[probeCount];
         probeMean = new double[probeCount];
         probeVariance = new double[probeCount];
 
         for (int f = 0; f < probeCount; ++f) {
             double[] probeData = getProbeData(f);
-            probeOriginalMean[f] = Descriptives.mean(probeData);
-            probeOriginalVariance[f] = Descriptives.variance(probeData, probeMean[f]);
             probeMean[f] = Descriptives.mean(probeData);
             probeVariance[f] = Descriptives.variance(probeData, probeMean[f]);
         }
     }
 
+    private void calcMeanAndVarianceOrg() {
+        //Precalculate means and variances. This will improve calculations substantially:
+        int probeCount = probes.length;
+        probeOriginalMean = new double[probeCount];
+        probeOriginalVariance = new double[probeCount];
+
+        for (int f = 0; f < probeCount; ++f) {
+            double[] probeData = getProbeData(f);
+            probeOriginalMean[f] = Descriptives.mean(probeData);
+            probeOriginalVariance[f] = Descriptives.variance(probeData, probeOriginalMean[f]);
+        }
+    }
+    
     private double[] getProbeData(int f) {
         double[] probeData = new double[individuals.length];
         System.arraycopy(matrix[f], 0, probeData, 0, individuals.length);
