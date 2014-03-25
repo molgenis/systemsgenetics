8f81e82bddd14e286f6f6330f67e00eef1f5787f
Patrick Deelen <patrickdeelen@gmail.com>
2014-03-25 10:27:33 +0100

    move test

---
+++ eqtl-mapping-pipeline/src/test/java/eqtlmappingpipeline/NormalizationTest.java
@@ -0,0 +1,28 @@
+package eqtlmappingpipeline;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static org.testng.Assert.*;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ *
+ * @author MarcJan
+ */
+public class NormalizationTest {
+    
+    public NormalizationTest() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+}
