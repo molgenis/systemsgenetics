ed71a148c41ce3ad6d8c7e5c1043bcec5e0d3034
Patrick Deelen <patrickdeelen@gmail.com>
2014-03-25 10:27:26 +0100

    QTL smoothsorttest

---
+++ eqtl-mapping-pipeline/src/test/java/eqtlmappingpipeline/SmoothSortTestQtl.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eqtlmappingpipeline;
+
+import eqtlmappingpipeline.metaqtl3.containers.QTL;
+import java.util.Random;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import umcg.genetica.util.SmoothSort;
+import static org.testng.Assert.*;
+
+/**
+ *
+ * @author MarcJan
+ */
+public class SmoothSortTestQtl {
+
+	private static final Random random = new Random(1);
+	
+	public SmoothSortTestQtl() {
+	}
+
+	@BeforeMethod
+	public void setUpMethod() throws Exception {
+	}
+
+	/**
+	 * Test of sort method, of class SmoothSort.
+	 */
+	@Test
+	public void testSortBig() {
+
+		QTL[] eqtls = new QTL[100000];
+		
+		int numberToSort = 90000;
+
+		for (int i = 0; i < eqtls.length ; ++i) {
+			
+			double pvalue = random.nextDouble();
+			double zscore = random.nextDouble();
+			
+			eqtls[i] = new QTL(pvalue, 0, 0, (byte) 0, zscore, null, null, null, null, null, null, null, 0d, 0d);
+		}
+		
+		
+		SmoothSort.sort(eqtls, 0, numberToSort);
+		
+		double lastPvalue = Double.MIN_VALUE;
+		
+		for (int i = 0; i < numberToSort ; ++i) {
+			
+			assertFalse(eqtls[i].getPvalue() < lastPvalue, "Index " + i + " last p-value " + lastPvalue + " current p-value " + eqtls[i].getPvalue());
+			lastPvalue = eqtls[i].getPvalue();
+			
+		}
+		
+		
+		
+	}
+}
