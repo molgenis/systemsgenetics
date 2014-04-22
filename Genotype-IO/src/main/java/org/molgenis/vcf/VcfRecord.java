3eed1e6d46331159f9e2bc7b771f11fd915dd727
dennishendriksen <d.hendriksen@umcg.nl>
2014-04-22 14:42:28 +0200

    vcf parser without molgenis/genotypeio dependencies

---
+++ Genotype-IO/src/main/java/org/molgenis/vcf/VcfRecord.java
@@ -0,0 +1,207 @@
+package org.molgenis.vcf;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.molgenis.vcf.meta.VcfMeta;
+
+public class VcfRecord
+{	
+	private final VcfMeta vcfMeta;
+	private String[] tokens;
+
+	private transient List<String> cachedIdentifiers; 		
+	private transient List<String> cachedAlternateAlleles;
+	private transient String[] cachedSampleDataTypes;
+	
+	public VcfRecord(VcfMeta vcfMeta) {
+		this(vcfMeta, null);
+	}
+	
+	public VcfRecord(VcfMeta vcfMeta, String[] tokens)
+	{
+		if(vcfMeta == null) throw new IllegalArgumentException("colNames is null");
+		this.vcfMeta = vcfMeta;
+		this.tokens = tokens;
+	}
+	
+	public String getChromosome() {
+		return tokens[VcfMeta.COL_CHROM_IDX];
+	}
+	
+	public int getPosition() {
+		return Integer.valueOf(tokens[VcfMeta.COL_POS_IDX]);
+	}
+	
+	public List<String> getIdentifiers() {
+		if(cachedIdentifiers == null) {
+			String identifiersStr = tokens[VcfMeta.COL_ID_IDX];
+			
+			if (identifiersStr == null) {
+				cachedIdentifiers = Collections.emptyList();
+			} else {
+				cachedIdentifiers = Arrays.asList(StringUtils.split(identifiersStr, ';'));
+			}
+		}
+		return cachedIdentifiers;
+	}
+	
+	public String getReferenceAllele() {
+		return tokens[VcfMeta.COL_REF_IDX];
+	}
+	
+	public List<String> getAlternateAlleles() {
+		if(cachedAlternateAlleles == null) {
+			String alternateBasesStr = tokens[VcfMeta.COL_ALT_IDX];
+			if(alternateBasesStr.length() == 0) {
+				cachedAlternateAlleles = Collections.emptyList();
+			} else {
+				cachedAlternateAlleles = Arrays.asList(StringUtils.split(alternateBasesStr, ','));
+			}
+		}
+		return cachedAlternateAlleles;
+	}
+	
+	public String getQuality() {
+		return tokens[VcfMeta.COL_QUAL_IDX];
+	}
+	
+	public String getFilterStatus() {
+		return tokens[VcfMeta.COL_FILTER_IDX];
+	}
+	
+	public Iterable<VcfInfo> getInformation() {
+		final String[] infoTokens = StringUtils.split(tokens[VcfMeta.COL_INFO_IDX], ';');
+		return new Iterable<VcfInfo>() {
+			
+			@Override
+			public Iterator<VcfInfo> iterator()
+			{
+				return new Iterator<VcfInfo>(){
+					private final VcfInfo recycableVcfInfo = new VcfInfo(vcfMeta);
+					private int nrToken = 0; 
+					
+					@Override
+					public boolean hasNext()
+					{
+						return nrToken < infoTokens.length;
+					}
+
+					@Override
+					public VcfInfo next()
+					{
+						String infoToken = infoTokens[nrToken++];
+						int idx = infoToken.indexOf('=');
+						recycableVcfInfo.reset(infoToken.substring(0, idx), infoToken.substring(idx + 1));
+						return recycableVcfInfo;
+					}
+
+					@Override
+					public void remove()
+					{
+						throw new UnsupportedOperationException();
+					}};
+			}
+		};
+	}
+	
+	public String[] getFormat() {
+		if(cachedSampleDataTypes == null) {
+			cachedSampleDataTypes = StringUtils.split(tokens[VcfMeta.COL_FORMAT_IDX], ':'); 
+		}
+		return cachedSampleDataTypes;
+	}
+	
+	public int getFormatIndex(String dataType) {
+		String[] dataTypes = getFormat();
+		for(int i = 0; i < dataTypes.length; ++i)
+			if(dataTypes[i].equals(dataType))
+				return i;
+		return -1;
+	}
+	
+	public int getNrSamples() {
+		return tokens.length > VcfMeta.COL_FORMAT_IDX + 1 ? tokens.length - (VcfMeta.COL_FORMAT_IDX + 1) : 0;
+	}
+	
+	public Iterable<VcfSample> getSamples() {
+		final VcfRecord vcfRecord = this;
+		return new Iterable<VcfSample>() {
+
+			@Override
+			public Iterator<VcfSample> iterator()
+			{
+				return new Iterator<VcfSample>(){
+					private final VcfSample recycableVcfSample = new VcfSample(vcfRecord);
+					
+					private final int nrSamples = getNrSamples();
+					private int nrSample = 0;
+					
+					@Override
+					public boolean hasNext()
+					{
+						return nrSample < nrSamples;
+					}
+
+					@Override
+					public VcfSample next()
+					{
+						recycableVcfSample.reset(StringUtils.split(tokens[VcfMeta.COL_FORMAT_IDX + 1 + nrSample], ':'));
+						++nrSample;
+						return recycableVcfSample;
+					}
+
+					@Override
+					public void remove()
+					{
+						throw new UnsupportedOperationException();
+					}};
+			}
+		};
+	}
+
+	public void reset(String[] tokens) {
+		this.tokens = tokens;
+		this.cachedIdentifiers = null;
+		this.cachedAlternateAlleles = null;
+		this.cachedSampleDataTypes = null;
+	}
+	
+	public VcfRecord createClone() {
+		return new VcfRecord(vcfMeta, tokens);
+	}
+	
+	@Override
+	public String toString() {
+		return StringUtils.join(tokens, '\t');
+	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(tokens);
+		result = prime * result + ((vcfMeta == null) ? 0 : vcfMeta.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		VcfRecord other = (VcfRecord) obj;
+		if (!Arrays.equals(tokens, other.tokens)) return false;
+		if (vcfMeta == null)
+		{
+			if (other.vcfMeta != null) return false;
+		}
+		else if (!vcfMeta.equals(other.vcfMeta)) return false;
+		return true;
+	}
+}
---
+++ Genotype-IO/src/main/java/org/molgenis/vcf/VcfSample.java
@@ -0,0 +1,107 @@
+package org.molgenis.vcf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VcfSample
+{
+	private static final String FIELD_GT = "GT";
+	private static final char GENOTYPE_UNPHASED = '/';
+	private static final char GENOTYPE_PHASED = '|';
+	
+	private final VcfRecord vcfRecord;
+	private String[] tokens;
+
+	public VcfSample(VcfRecord vcfRecord) {
+		this(vcfRecord, null);
+	}
+	
+	public VcfSample(VcfRecord vcfRecord, String[] tokens) {
+		if(vcfRecord == null) throw new IllegalArgumentException("vcfRecord is null");
+		this.vcfRecord = vcfRecord;
+		this.tokens = tokens;
+	}
+	
+	public String getData(int idx) {
+		return tokens[idx];
+	}
+	
+	public List<Boolean> getPhasings() {
+		// the first sub-field must always be the genotype if it is present
+		String[] dataTypes = vcfRecord.getFormat();
+		if(dataTypes.length == 0 || !dataTypes[0].equals(FIELD_GT)) return null;
+		String genotype = tokens[0];
+		
+		// parse phasing
+		List<Boolean> phasings = new ArrayList<Boolean>(1);
+		final int nrChars = genotype.length();
+		for (int i = 0; i < nrChars; ++i){
+			switch(genotype.charAt(i)) {
+				case GENOTYPE_PHASED:
+					phasings.add(Boolean.TRUE);
+					break;
+				case GENOTYPE_UNPHASED:
+					phasings.add(Boolean.FALSE);
+					break;
+				default:
+					break;
+			}
+		}
+		return phasings;
+	}
+	
+	public List<String> getAlleles() {
+		// the first sub-field must always be the genotype if it is present
+		String[] dataTypes = vcfRecord.getFormat();
+		if(dataTypes.length == 0 || !dataTypes[0].equals(FIELD_GT)) return null;
+		String genotype = tokens[0];
+		
+		String referenceAllele = vcfRecord.getReferenceAllele();
+		List<String> alternateAlleles = vcfRecord.getAlternateAlleles();
+		
+		// performance optimization for the common case that a sample consists of two alleles
+		List<String> alleles = new ArrayList<String>(2);
+		final int nrGenotypeChars = genotype.length();
+		for (int j = 0, start = 0; j < nrGenotypeChars; ++j)
+		{
+			char c = genotype.charAt(j);
+			if (c == GENOTYPE_PHASED || c == GENOTYPE_UNPHASED || j == nrGenotypeChars - 1)
+			{
+				if(j - start == 1) {
+					// performance optimization for the common case that an allele is described by one char
+					char alleleChar = j == nrGenotypeChars - 1 ?  c : genotype.charAt(j - 1);
+					if(alleleChar != '.') {
+						int alleleIndex = Character.digit(alleleChar, 10);
+						if(alleleIndex == 0)
+							alleles.add(referenceAllele);
+						else
+							alleles.add(alternateAlleles.get(alleleIndex - 1));
+					} else {
+						alleles.add(null);
+					}
+				} else {
+					String alleleIndexStr = j == nrGenotypeChars - 1 ? genotype.substring(start) : genotype.substring(start, j);
+					if (!alleleIndexStr.equals(".")) {
+						int alleleIndex = Integer.parseInt(alleleIndexStr);
+						if(alleleIndex == 0)
+							alleles.add(referenceAllele);
+						else
+							alleles.add(alternateAlleles.get(alleleIndex - 1));
+					} else {
+						alleles.add(null);
+					}
+				}
+				start = j + 1;
+			}
+		}
+		return alleles;
+	}
+	
+	public VcfSample createClone() {
+		return new VcfSample(vcfRecord, tokens);
+	}
+	
+	public void reset(String[] tokens) {
+		this.tokens = tokens;
+	}
+}
