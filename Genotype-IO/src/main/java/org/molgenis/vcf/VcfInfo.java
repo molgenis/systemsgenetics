a4c38c70c33dc9b912afcf0227d72a0b965cdab8
dennishendriksen <d.hendriksen@umcg.nl>
2014-04-25 08:10:02 +0200

    fix imports

--- Genotype-IO/src/main/java/org/molgenis/vcf/VcfInfo.java
+++ Genotype-IO/src/main/java/org/molgenis/vcf/VcfInfo.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.molgenis.vcf.meta.VcfMeta;
 import org.molgenis.vcf.meta.VcfMetaInfo;
 
--- Genotype-IO/src/main/java/org/molgenis/vcf/VcfRecord.java
+++ Genotype-IO/src/main/java/org/molgenis/vcf/VcfRecord.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.molgenis.vcf.meta.VcfMeta;
 
 public class VcfRecord
