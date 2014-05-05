d54d927afd341cce3dbccf0d8e36c6b66728beaf
Patrick Deelen <patrickdeelen@gmail.com>
2014-05-05 21:38:38 +0200

    Working on parsing pileup files

---
+++ eqtl-mapping-pipeline/src/main/java/eqtlmappingpipeline/ase/pileup/pileupToVcf.java
@@ -0,0 +1,18 @@
+package eqtlmappingpipeline.ase.pileup;
+
+/**
+ *
+ * @author Patrick Deelen
+ */
+public class pileupToVcf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+		
+		
+    }
+
+}
