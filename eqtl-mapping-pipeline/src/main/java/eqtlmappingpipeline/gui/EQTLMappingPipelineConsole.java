186c26c615f5930b4ba7be3858d56ed2762e3b5b
Patrick Deelen <patrickdeelen@gmail.com>
2014-06-26 10:47:26 +0200

    Intergrated ASE in --mode system of qtl mapping pipeline. Fixed some typos

--- eqtl-mapping-pipeline/src/main/java/eqtlmappingpipeline/gui/EQTLMappingPipelineConsole.java
+++ eqtl-mapping-pipeline/src/main/java/eqtlmappingpipeline/gui/EQTLMappingPipelineConsole.java
@@ -20,7 +20,6 @@ import eqtlmappingpipeline.qcpca.QCPCAConsoleGui;
 import eqtlmappingpipeline.util.UtilConsoleGUI;
 import eqtlmappingpipeline.util.eQTLFileCompare;
 import java.util.Arrays;
-import org.apache.commons.cli.ParseException;
 import umcg.genetica.console.ConsoleGUIElems;
 import umcg.genetica.io.pileup.PileupToVcf;
 
@@ -31,113 +30,115 @@ import umcg.genetica.io.pileup.PileupToVcf;
  */
 public class EQTLMappingPipelineConsole {
 
-    public void main(String[] args) throws Exception {
+	public void main(String[] args) throws Exception {
 
-        if (args == null || args.length == 0) {
-            
-            printHeader();
-            printUsage();
-            System.out.println("\nERROR: Please supply --mode\n");
-            return;
-        }
-        if (args[0].equals("--ase")) {
-            Ase.main(Arrays.copyOfRange(args, 1, args.length));
-            return;
-        }
-        if (args[0].equals("--pilupToVcf")) {
-            PileupToVcf.main(Arrays.copyOfRange(args, 1, args.length));
-            return;
-        }
+		if (args == null || args.length == 0) {
 
-        String mode = null;
+			printHeader();
+			printUsage();
+			System.out.println("\nERROR: Please supply --mode\n");
+			return;
+		}
 
-        for (int i = 0; i < args.length; i++) {
-            String arg = args[i];
-            String val = null;
+		String mode = null;
 
-            if (i + 1 < args.length) {
-                val = args[i + 1];
-            }
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
+			String val = null;
 
-            if (arg.equals("--imputationtool")) {
+			if (i + 1 < args.length) {
+				val = args[i + 1];
+			}
 
-                imputationtool.ImputationTool.main(args);
-            }
+			if (arg.equals("--imputationtool")) {
 
-            if (arg.equals("--metamode")) {
-                eqtlmappingpipeline.binarymeta.Main.main(args);
-                System.exit(0);
-            }
+				imputationtool.ImputationTool.main(args);
+			}
 
-            if (arg.equals("--mode")) {
-                mode = val;
-            }
-        }
+			if (arg.equals("--metamode")) {
+				eqtlmappingpipeline.binarymeta.Main.main(args);
+				System.exit(0);
+			}
 
-        if (mode == null) {
-            System.out.println("ERROR: Please supply --mode");
-            printUsage();
-        } else {
-            if (mode.equals("metaqtl")) {
-                MetaQTL3ConsoleGUI metaQTL = new MetaQTL3ConsoleGUI(args);
-            } else if (mode.equals("metaqtl4")) {
-                MetaQTL4ConsoleUI mui = new MetaQTL4ConsoleUI(args);
-            } else if (mode.equals("mixupmapper")) {
-                MixupMapperConsoleGUI m = new MixupMapperConsoleGUI(args);
-            } else if (mode.equals("normalize")) {
-                NormalizationConsoleGUI p = new NormalizationConsoleGUI(args);
-            } else if (mode.equals("compare")) {
-                eQTLFileCompare r = new eQTLFileCompare(args);
-            } else if (mode.equals("chryplot")) {
-                ChrYExpressionPlotConsoleGUI r = new ChrYExpressionPlotConsoleGUI(args);
-            } else if (mode.equals("pcaoptimum")) {
-                PCAOptimumConsoleGUI g = new PCAOptimumConsoleGUI(args);
-            } else if (mode.equals("foldchange")) {
-                eQTLFoldChangeCalculatorGUI g = new eQTLFoldChangeCalculatorGUI(args);
-            } else if (mode.equals("util")) {
-                UtilConsoleGUI g = new UtilConsoleGUI(args);
-            } else if (mode.equals("qcpca")) {
-                QCPCAConsoleGui q = new QCPCAConsoleGui(args);
-            } else if (mode.equals("conditional")) {
-                ConditionalAnalysisConsoleGUI q = new ConditionalAnalysisConsoleGUI(args);
-            } else if (mode.equals("iv")) {
-                IVConsoleGUI q = new IVConsoleGUI(args);
-            } else if (mode.equals("celltypespecific")) {
-                InteractionAnalysisConsoleGUI q = new InteractionAnalysisConsoleGUI(args);
-            } else {
-                printUsage();
-            }
-        }
+			if (arg.equals("--mode")) {
+				mode = val;
+			}
+		}
 
-        System.out.println(ConsoleGUIElems.DOUBLELINE);
-    }
+		if (mode == null) {
+			System.out.println("ERROR: Please supply --mode");
+			printUsage();
+		} else {
+			if (mode.equals("metaqtl")) {
+				MetaQTL3ConsoleGUI metaQTL = new MetaQTL3ConsoleGUI(args);
+			} else if (mode.equals("metaqtl4")) {
+				MetaQTL4ConsoleUI mui = new MetaQTL4ConsoleUI(args);
+			} else if (mode.equals("mixupmapper")) {
+				MixupMapperConsoleGUI m = new MixupMapperConsoleGUI(args);
+			} else if (mode.equals("normalize")) {
+				NormalizationConsoleGUI p = new NormalizationConsoleGUI(args);
+			} else if (mode.equals("compare")) {
+				eQTLFileCompare r = new eQTLFileCompare(args);
+			} else if (mode.equals("chryplot")) {
+				ChrYExpressionPlotConsoleGUI r = new ChrYExpressionPlotConsoleGUI(args);
+			} else if (mode.equals("pcaoptimum")) {
+				PCAOptimumConsoleGUI g = new PCAOptimumConsoleGUI(args);
+			} else if (mode.equals("foldchange")) {
+				eQTLFoldChangeCalculatorGUI g = new eQTLFoldChangeCalculatorGUI(args);
+			} else if (mode.equals("util")) {
+				UtilConsoleGUI g = new UtilConsoleGUI(args);
+			} else if (mode.equals("qcpca")) {
+				QCPCAConsoleGui q = new QCPCAConsoleGui(args);
+			} else if (mode.equals("conditional")) {
+				ConditionalAnalysisConsoleGUI q = new ConditionalAnalysisConsoleGUI(args);
+			} else if (mode.equals("iv")) {
+				IVConsoleGUI q = new IVConsoleGUI(args);
+			} else if (mode.equals("celltypespecific")) {
+				InteractionAnalysisConsoleGUI q = new InteractionAnalysisConsoleGUI(args);
+			} else if (mode.equals("ase")) {
+				Ase.main(Arrays.copyOfRange(args, 2, args.length));
+				return;
+			} else if (mode.equals("pileupToVcf")) {
+				PileupToVcf.main(Arrays.copyOfRange(args, 2, args.length));
+				return;
+			} else {
+				printUsage();
+			}
+		}
 
-    private void printHeader() {
+		System.out.println(ConsoleGUIElems.DOUBLELINE);
+	}
 
-        //Note: Version is null when running from netbeans but will be set when buidling a jar
-        System.out.println("\n"
-                + ConsoleGUIElems.DOUBLELINE
-                + "Version: " + Main.VERSION + "\n"
-                + "Department of Genetics, University Medical Centre Groningen\n"
-                + "Harm-Jan Westra, Patrick Deelen, Marc Jan Bonder, Dasha Zhernakova and Lude Franke\n"
-                + ConsoleGUIElems.DOUBLELINE + "\n");
-    }
+	private void printHeader() {
 
-    /*
-     * prints usage
-     */
-    private void printUsage() {
-        System.out.println("");
-        System.out.print("\nCommand line options\n" + ConsoleGUIElems.LINE);
-        System.out.println("       metaqtl\t\tPerform QTL mapping on the dataset\n"
-                + "       mixupmapper\tPerform MixUp mapping on the dataset\n"
-                + "       normalize\tPerform and Remove principal components from expression data\n"
-                + "       pcaoptimum\tDetermine optimum PCs to subtract from the data\n"
-                + "       qcpca\t\tPerform principal component analysis on both genotype and gene expression data\n"
-                + "       conditional\tPerform conditional eQTL analysis given a set of SNPs\n"
-                + "       iv\t\tPerform instrumental variable analysis\n"
-                + "       celltypespecific\tCell type specific eQTL mapping");
-        System.out.println("");
+		//Note: Version is null when running from netbeans but will be set when buidling a jar
+		System.out.println("\n"
+				+ ConsoleGUIElems.DOUBLELINE
+				+ "Version: " + Main.VERSION + "\n"
+				+ "Department of Genetics, University Medical Center Groningen\n"
+				+ "www.molgenis.org/systemsgenetics\n"
+				+ "Harm-Jan Westra, Patrick Deelen, Marc Jan Bonder, Dasha Zhernakova and Lude Franke\n"
+				+ ConsoleGUIElems.DOUBLELINE + "\n");
+	}
 
-    }
+	/*
+	 * prints usage
+	 */
+	private void printUsage() {
+		System.out.println("");
+		System.out.print("\nCommand line options\n" + ConsoleGUIElems.LINE);
+		System.out.println("       metaqtl\t\tPerform QTL mapping on the dataset\n"
+				+ "       mixupmapper\tPerform MixUp mapping on the dataset\n"
+				+ "       normalize\tPerform and Remove principal components from expression data\n"
+				+ "       pcaoptimum\tDetermine optimum PCs to subtract from the data\n"
+				+ "       qcpca\t\tPerform principal component analysis on both genotype and gene expression data\n"
+				+ "       conditional\tPerform conditional eQTL analysis given a set of SNPs\n"
+				+ "       iv\t\tPerform instrumental variable analysis\n"
+				+ "       celltypespecific\tCell type specific eQTL mapping\n"
+				+ "       ase\t\tAllele Specific Expression mapping\n"
+				+ "       pileupToVcf\tConvert a pileup file to vcf for usage in ASE mapping");
+		System.out.println("");
+		System.out.println("More information: www.molgenis.org/systemsgenetics");
+
+	}
 }
