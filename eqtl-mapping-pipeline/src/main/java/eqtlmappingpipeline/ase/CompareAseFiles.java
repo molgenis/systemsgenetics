bcd613097bc75918d3b871e85687530e44ebbf21
Marc Jan Bonder <bonder.m.j@gmail.com>
2014-07-16 15:54:17 +0200

    Compare AseFiles

--- eqtl-mapping-pipeline/src/main/java/eqtlmappingpipeline/ase/CompareAseFiles.java
+++ eqtl-mapping-pipeline/src/main/java/eqtlmappingpipeline/ase/CompareAseFiles.java
@@ -10,10 +10,8 @@ import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.regex.Pattern;
 import umcg.genetica.io.trityper.EQTL;
-import umcg.genetica.io.trityper.eQTLTextFile;
 
 /**
  *
@@ -38,79 +36,58 @@ public class CompareAseFiles {
 		BufferedReader aseReader = new BufferedReader(new FileReader("D:\\ASEcheck\\geuvadis_maskAll4_r20_a10_p2_s1_rq17_m1_gatkGenoGq30\\ase_bh.txt"));
         BufferedReader aseReader2 = new BufferedReader(new FileReader("D:\\ASEcheck\\all_maskAll4_r20_a10_p2_s5_rq17_m1_gatkGenoGq30\\ase_bh.txt"));
         
-		HashMap<String, ArrayList<AseVariant>> AseEffects = new HashMap<String, ArrayList<AseVariant>>();
+        HashMap<String, AseVariantBean> aseEffects = new HashMap<String, AseVariantBean>();
+        HashSet<String> totalEffects = new HashSet<String>();
 
         String line;
+        String[] elements;
+        //Header
+        aseReader.readLine();
 		while ((line = aseReader.readLine()) != null) {
-			String eQtlKey = eQtl.getRsChr() + ":" + eQtl.getRsChrPos();
-			ArrayList<EQTL> posEqtls = eQtls.get(eQtlKey);
-			if (posEqtls == null) {
-				posEqtls = new ArrayList<EQTL>(1);
-				eQtls.put(eQtlKey, posEqtls);
-			}
-			posEqtls.add(eQtl);
+            elements = TAB_PATTERN.split(line);
+			aseEffects.put(elements[7],new AseVariantBean(elements));
 		}
-
-		int aseTotal = 0;
-		int aseWithEQtl = 0;
+        aseReader.close();
+        
+        
+		int aseTotalInFile2 = 0;
+		int asePresentInBoth = 0;
 		int sameDirection = 0;
 		int oppositeDirection = 0;
 
-		HashSet<String> countedGenes = new HashSet<String>();
-
-		aseReader.readLine();//header
-		String line;
-		String[] elements;
-		while ((line = aseReader.readLine()) != null) {
-
-			elements = TAB_PATTERN.split(line);
-
-
-			HashSet<String> aseGenes = new HashSet<String>();
-			for (String gene : COMMA_PATTERN.split(elements[ASE_GENES_COLUMN])) {
-				aseGenes.add(gene);
-			}
-
-			++aseTotal;
-
-			ArrayList<EQTL> posEqtls = eQtls.get(elements[ASE_CHR_COLUMN] + ":" + elements[ASE_POS_COLUMN]);
-			if (posEqtls != null) {
-				for (EQTL eQtl : posEqtls) {
-					if (eQtl != null && aseGenes.contains(eQtl.getProbe())) {
-
-						if (countedGenes.contains(eQtl.getProbe())) {
-							continue;
-						}
-						countedGenes.add(eQtl.getProbe());
-
-						//System.out.println(eQtl.getProbe());
-
-						//if(eQtl.getRsChr() == 6 && eQtl.getRsChrPos() > 20000000 && eQtl.getRsChrPos() < 40000000) { continue; }
-
-						++aseWithEQtl;
-
-
-						double aseEstimate = Double.parseDouble(elements[ASE_ESTIMATE_COLUMN]);
-						double eQtlZ = elements[ASE_A1_COLUMN].equals(eQtl.getAlleleAssessed()) ? eQtl.getZscore() : eQtl.getZscore() * -1;
-
-						if (aseEstimate > 0.5 && eQtlZ > 0 || aseEstimate < 0.5 && eQtlZ < 0) {
-							++sameDirection;
-						} else {
-							System.out.println("Opposite: " + eQtl.getRsChr() + ":" + eQtl.getRsChrPos() + "\t" + elements[ASE_A1_COLUMN] + "\t" + eQtl.getAlleleAssessed() + "\t" + aseEstimate + "\t" + eQtl.getZscore());
-							++oppositeDirection;
-						}
-					}
-				}
-			}
+		aseReader2.readLine();//header
+		line = null;
+		elements = null;
+		while ((line = aseReader2.readLine()) != null) {
+            ++aseTotalInFile2;
+			elements = TAB_PATTERN.split(line);	
+            AseVariantBean currentEffect = new AseVariantBean(elements);
+            totalEffects.add(currentEffect.getId().toString());
+            
+            if(aseEffects.containsKey(currentEffect.getId().toString())){
+                AseVariantBean otherEffect =  aseEffects.get(currentEffect.getId().toString());
+                asePresentInBoth++;
+                if(otherEffect.getEffect()<0.5 && currentEffect.getEffect()<0.5){
+                    sameDirection++;
+                } else if(otherEffect.getEffect()>0.5 && currentEffect.getEffect()>0.5){
+                    sameDirection++;
+                } else {
+                    oppositeDirection++;
+                }
+            }
 		}
-
+        totalEffects.addAll(aseEffects.keySet());
+        aseReader2.close();
+        
 		NumberFormat numberFormat = NumberFormat.getInstance();
 		numberFormat.setMinimumFractionDigits(2);
 		numberFormat.setMaximumFractionDigits(2);
-		System.out.println("Ase total: " + aseTotal);
-		System.out.println("Ase SNP with eQTL effect: " + aseWithEQtl + " (" + numberFormat.format(aseWithEQtl / (double) aseTotal) + ")");
-		System.out.println(" - Same direction: " + sameDirection + " (" + numberFormat.format(sameDirection / (double) aseWithEQtl) + ")");
-		System.out.println(" - Opposite direction: " + oppositeDirection + " (" + numberFormat.format(oppositeDirection / (double) aseWithEQtl) + ")");
+		System.out.println("Ase effects in file 1: " + aseEffects.size());
+        System.out.println("Ase effects in file 2: " + aseTotalInFile2);
+        System.out.println("Total unique Ase effects observed : " + totalEffects.size());
+		System.out.println("Ase effects identified in both analyses: " + asePresentInBoth + " (" + numberFormat.format(asePresentInBoth / (double) totalEffects.size()) + ")");
+		System.out.println(" - Same direction: " + sameDirection + " (" + numberFormat.format(sameDirection / (double) asePresentInBoth) + ")");
+		System.out.println(" - Opposite direction: " + oppositeDirection + " (" + numberFormat.format(oppositeDirection / (double) asePresentInBoth) + ")");
 
 	}
 }
