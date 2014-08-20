d403a8c29d3a7f5fbb23c4af87f37323a3b98c89
Marc Jan Bonder <bonder.m.j@gmail.com>
2014-08-20 11:18:32 +0200

    Bug fixes noLdSnpProbeList creator

--- eqtl-mapping-pipeline/src/main/java/eqtlmappingpipeline/util/NoLdSnpProbeListCreator.java
+++ eqtl-mapping-pipeline/src/main/java/eqtlmappingpipeline/util/NoLdSnpProbeListCreator.java
@@ -43,18 +43,18 @@ import org.molgenis.genotype.variantFilter.VariantQcChecker;
  */
 public class NoLdSnpProbeListCreator {
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException, IOException, Exception {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException, IOException, Exception {
         CommandLineParser parser = new GnuParser();
-        
+
         System.out.flush(); //flush to make sure header is before errors
-		try {
-			Thread.sleep(25); //Allows flush to complete
-		} catch (InterruptedException ex) {
-		}
-        
+        try {
+            Thread.sleep(25); //Allows flush to complete
+        } catch (InterruptedException ex) {
+        }
+
         Options options = new Options();
 
         Option FileOut = OptionBuilder.withArgName("path").hasArg().withDescription("Location and name of the output file.").withLongOpt("OutputFile").create("o");
@@ -68,38 +68,38 @@ public class NoLdSnpProbeListCreator {
         Option variantFilter = OptionBuilder.withArgName("String").hasArg().withDescription("List with variant ids to select.").withLongOpt("variant_filter").create("vf");
         Option callRate = OptionBuilder.withArgName("int").hasArg().withDescription("Call-rate cut-off.").withLongOpt("min_callRate").create("vc");
         Option mafFilter = OptionBuilder.withArgName("int").hasArg().withDescription("Minor allel cut-off filter.").withLongOpt("min_maf").create("mf");
-        Option chrFilter = OptionBuilder.withArgName("string").hasArg().withDescription("Filter input data on chromosome").withLongOpt("chrFilter").create("ch");        
+        Option chrFilter = OptionBuilder.withArgName("string").hasArg().withDescription("Filter input data on chromosome").withLongOpt("chrFilter").create("ch");
         options.addOption(FileOut).addOption(RefferenceTypeIn).addOption(RefferenceIn).addOption(WindowSize).addOption(BedIn).addOption(ProbeMargin).addOption(MaxDprime).addOption(MaxRsquare).addOption(variantFilter).addOption(callRate).addOption(mafFilter).addOption(chrFilter);
-        
-		File probeFile = null;
-		String genotypePath = null;
-		String genotypeType = null;
-		int windowHalfSize = 250000;
-		int probeMargin = 0;
+
+        File probeFile = null;
+        String genotypePath = null;
+        String genotypeType = null;
+        int windowHalfSize = 250000;
+        int probeMargin = 0;
         float cRate = 0.0f;
         float maf = 0.0f;
         double HWE = 0.0d;
         String variantFilterList = null;
-		double maxDprime = 0.2;
-		double maxR2 = 0.2;
+        double maxDprime = 0.2;
+        double maxR2 = 0.2;
         String chrF = null;
-		File outputFile = null;
-        
-        
+        File outputFile = null;
+
+
         CommandLine cmd;
         try {
             cmd = parser.parse(options, args);
             HelpFormatter formatter = new HelpFormatter();
-            
+
             if (cmd.hasOption("OutputFile") || cmd.hasOption("o")) {
-            // initialise the member variable
+                // initialise the member variable
                 outputFile = new File(cmd.getOptionValue("OutputFile"));
             } else {
                 System.out.println("Missing necesarray information");
                 formatter.printHelp("ant", options);
                 System.exit(0);
             }
-            if (cmd.hasOption("RefferenceLocation")|| cmd.hasOption("ri")) {
+            if (cmd.hasOption("RefferenceLocation") || cmd.hasOption("ri")) {
                 // initialise the member variable
                 genotypePath = cmd.getOptionValue("RefferenceLocation");
             } else {
@@ -107,7 +107,7 @@ public class NoLdSnpProbeListCreator {
                 formatter.printHelp("ant", options);
                 System.exit(0);
             }
-            if (cmd.hasOption("RefferenceType")|| cmd.hasOption("rt")) {
+            if (cmd.hasOption("RefferenceType") || cmd.hasOption("rt")) {
                 // initialise the member variable
                 genotypeType = cmd.getOptionValue("RefferenceType");
             } else {
@@ -116,59 +116,59 @@ public class NoLdSnpProbeListCreator {
                 System.exit(0);
             }
             if (cmd.hasOption("input") || cmd.hasOption("i")) {
-            // initialise the member variable
+                // initialise the member variable
                 probeFile = new File(cmd.getOptionValue("input"));
             } else {
                 System.out.println("Missing necesarray information");
                 formatter.printHelp("ant", options);
                 System.exit(0);
             }
-            if (cmd.hasOption("Window_Size")|| cmd.hasOption("ws")) {
+            if (cmd.hasOption("Window_Size") || cmd.hasOption("ws")) {
                 // initialise the member variable
                 windowHalfSize = Integer.parseInt(cmd.getOptionValue("Window_Size"));
             }
-            if (cmd.hasOption("Probe_margin")|| cmd.hasOption("pm")) {
+            if (cmd.hasOption("Probe_margin") || cmd.hasOption("pm")) {
                 // initialise the member variable
                 probeMargin = Integer.parseInt(cmd.getOptionValue("Probe_margin"));
             }
-            if (cmd.hasOption("variant_filter")|| cmd.hasOption("vf")) {
+            if (cmd.hasOption("variant_filter") || cmd.hasOption("vf")) {
                 // initialise the member variable
                 variantFilterList = cmd.getOptionValue("variant_filter");
             }
-            if (cmd.hasOption("min_callRate")|| cmd.hasOption("vc")) {
+            if (cmd.hasOption("min_callRate") || cmd.hasOption("vc")) {
                 // initialise the member variable
                 cRate = Float.parseFloat(cmd.getOptionValue("min_callRate"));
             }
-            if (cmd.hasOption("min_maf")|| cmd.hasOption("mf")) {
+            if (cmd.hasOption("min_maf") || cmd.hasOption("mf")) {
                 // initialise the member variable
                 maf = Float.parseFloat(cmd.getOptionValue("min_maf"));
             }
-            if (cmd.hasOption("MaxDprime")|| cmd.hasOption("md")) {
+            if (cmd.hasOption("MaxDprime") || cmd.hasOption("md")) {
                 // initialise the member variable
                 maxDprime = Double.parseDouble(cmd.getOptionValue("MaxDprime"));
             }
-            if (cmd.hasOption("max_rSquare")|| cmd.hasOption("mr")) {
+            if (cmd.hasOption("max_rSquare") || cmd.hasOption("mr")) {
                 // initialise the member variable
                 maxR2 = Double.parseDouble(cmd.getOptionValue("max_rSquare"));
             }
-            if(cmd.hasOption("chrFilter") || cmd.hasOption("ch")){
+            if (cmd.hasOption("chrFilter") || cmd.hasOption("ch")) {
                 chrF = cmd.getOptionValue("chrFilter");
             }
-            
+
         } catch (ParseException ex) {
             Logger.getLogger(NoLdSnpProbeListCreator.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         SampleFilter sf = null;
-        
+
         VariantCombinedFilter varFilter = new VariantCombinedFilter();
         varFilter.add(new VariantFilterBiAllelic());
-        
-        if(cRate!=0.0f || maf!=0.0f || HWE!=0.0d){
+
+        if (cRate != 0.0f || maf != 0.0f || HWE != 0.0d) {
             VariantQcChecker qc = new VariantQcChecker(maf, cRate, HWE);
             varFilter.add(qc);
         }
-        if(variantFilterList!=null){
+        if (variantFilterList != null) {
             HashSet<String> snps = new HashSet<String>();
 
             BufferedReader variantIdFilterReader = new BufferedReader(new FileReader(variantFilterList));
@@ -184,83 +184,86 @@ public class NoLdSnpProbeListCreator {
             VariantFilterSeq seqFilter = new VariantFilterSeq(chrF);
             varFilter.add(seqFilter);
         }
-        
-		RandomAccessGenotypeData genotypeData = RandomAccessGenotypeDataReaderFormats.valueOf(genotypeType).createFilteredGenotypeData(genotypePath, 10000, varFilter, sf);
 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(probeFile), "UTF-8"));
-		final BufferedWriter snpProbeToTestWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile), "UTF-8"));
+        RandomAccessGenotypeData genotypeData = RandomAccessGenotypeDataReaderFormats.valueOf(genotypeType).createFilteredGenotypeData(genotypePath, 10000, varFilter, sf);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(probeFile), "UTF-8"));
+        final BufferedWriter snpProbeToTestWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile), "UTF-8"));
 
         int rSquareExclusions = 0;
         int dPrimeExclusions = 0;
         int possibleCombinations = 0;
         int locatedInProbe = 0;
-		String line;
-		String[] elements;
-		while ((line = reader.readLine()) != null) {
-			elements = StringUtils.splitPreserveAllTokens(line, '\t');
+        String line;
+        String[] elements;
+        while ((line = reader.readLine()) != null) {
+            elements = StringUtils.splitPreserveAllTokens(line, '\t');
 
-			if (elements.length < 4) {
-				throw new Exception();
-			}
+            if (elements.length < 4) {
+                throw new Exception();
+            }
 
-			String chr = elements[0].replace("chr", "");//TODO remove chr
-			int probeStartPos = Integer.parseInt(elements[1]);
-			int probeStopPos = Integer.parseInt(elements[2]);
-            int probeCenter = probeStartPos+(probeStopPos-probeStartPos/2);
-			String probeName = elements[3];
+            String chr = elements[0].replace("chr", "");
+            int probeStartPos = Integer.parseInt(elements[1]);
+            int probeStopPos = Integer.parseInt(elements[2]);
+            int probeCenter = probeStartPos + (int) (Math.floor((probeStopPos - probeStartPos) / 2.0d));
+            String probeName = elements[3];
 
-
-			int windowsStart = probeCenter - windowHalfSize;
+            int windowsStart = probeCenter - windowHalfSize;
             windowsStart = windowsStart < 0 ? 0 : windowsStart;
-			int windowStop = probeCenter + windowHalfSize;
+            int windowStop = probeCenter + windowHalfSize;
 
             ArrayList<GeneticVariant> probeVariants = Lists.newArrayList(genotypeData.getVariantsByRange(chr, (probeStartPos - probeMargin), (probeStopPos + probeMargin)));
-            
-			variants:
-			for (GeneticVariant variant : genotypeData.getVariantsByRange(chr, windowsStart, windowStop)) {
+
+            variants:
+            for (GeneticVariant variant : genotypeData.getVariantsByRange(chr, windowsStart, windowStop)) {
                 possibleCombinations++;
-				//skip over probe variants
-				if (variant.getStartPos() >= (probeStartPos - probeMargin) && variant.getStartPos() <= (probeStopPos + probeMargin)) {
+
+                //skip over probe variants
+                if (variant.getStartPos() >= (probeStartPos - probeMargin) && variant.getStartPos() <= (probeStopPos + probeMargin)) {
                     locatedInProbe++;
-					continue variants;
-				}
-                
-				for (GeneticVariant probeVariant : probeVariants) {
+                    continue variants;
+                }
 
-					Ld ld = variant.calculateLd(probeVariant);
+                if (!probeVariants.isEmpty()) {
 
-					if (ld.getDPrime() >= maxDprime) {
-						//Exclude
-                        dPrimeExclusions++;
-						continue variants;
-					}
-                    if( ld.getR2() >= maxR2){
-                        //Exclude
-                        rSquareExclusions++;
-                        continue variants;
+                    for (GeneticVariant probeVariant : probeVariants) {
+
+                        Ld ld = variant.calculateLd(probeVariant);
+
+                        if (ld.getDPrime() >= maxDprime) {
+                            //Exclude
+                            dPrimeExclusions++;
+                            continue variants;
+                        }
+                        if (ld.getR2() >= maxR2) {
+                            //Exclude
+                            rSquareExclusions++;
+                            continue variants;
+                        }
+
                     }
-
-				}
-
-				//Probe SNP combination is okay
+                }
+                //Probe SNP combination is okay
                 String variantPrimaryId = variant.getPrimaryVariantId();
-                if(variantPrimaryId==null){
+                if (variantPrimaryId == null) {
                     snpProbeToTestWriter.append(variant.getSequenceName()).append('-').append(String.valueOf(variant.getStartPos()));
                 } else {
                     snpProbeToTestWriter.append(variantPrimaryId);
                 }
-                
-				snpProbeToTestWriter.append('-');
-				snpProbeToTestWriter.append(probeName);
-				snpProbeToTestWriter.append('\n');
 
-			}
+                snpProbeToTestWriter.append('-');
+                snpProbeToTestWriter.append(probeName);
+                snpProbeToTestWriter.append('\n');
 
-		}
+            }
+
+        }
+
         snpProbeToTestWriter.close();
-        System.out.println("Number of valid start combination: "+ possibleCombinations);
-        System.out.println("Excluded because located in probe: "+ locatedInProbe);
-        System.out.println("Excluded based on D': "+dPrimeExclusions);
-        System.out.println("Excluded based on r2: "+rSquareExclusions);
-	}
+        System.out.println("Number of valid start combination: " + possibleCombinations);
+        System.out.println("Excluded because located in probe: " + locatedInProbe);
+        System.out.println("Excluded based on D': " + dPrimeExclusions);
+        System.out.println("Excluded based on r2: " + rSquareExclusions);
+    }
 }
