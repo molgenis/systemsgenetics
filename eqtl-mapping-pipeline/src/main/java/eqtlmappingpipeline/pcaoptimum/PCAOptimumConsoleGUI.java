413e2b9475a9c4e3d5d3b9c566a7816c5b2712dd
Marc Jan Bonder <bonder.m.j@gmail.com>
2014-09-17 09:50:57 +0200

    Version update
    Small updates to FDR advance

--- eqtl-mapping-pipeline/src/main/java/eqtlmappingpipeline/pcaoptimum/PCAOptimumConsoleGUI.java
+++ eqtl-mapping-pipeline/src/main/java/eqtlmappingpipeline/pcaoptimum/PCAOptimumConsoleGUI.java
@@ -231,7 +231,7 @@ public class PCAOptimumConsoleGUI {
                     p.setSNPSets(cissnps, transsnps);
                     p.setPerformpcqtlNormalization(performEigenvectorQTLMapping);
                     p.setCovariatesRemoved(covariatesremoved);
-                    p.initialize(settingsfile, settingstexttoreplace, settingstexttoreplacewith, null, null, in, inexp, inexpplatform, inexpannot, gte, out, cis, trans, perm, true, false, snpfile, threads, nrEQTLsToOutput, null, null, true, true, null, 0.05);
+                    p.initialize(settingsfile, settingstexttoreplace, settingstexttoreplacewith, null, null, in, inexp, inexpplatform, inexpannot, gte, out, cis, trans, perm, true, false, snpfile, threads, nrEQTLsToOutput, null, null, true, true, null, null);
                 }
             }
         } catch (Exception e) {
