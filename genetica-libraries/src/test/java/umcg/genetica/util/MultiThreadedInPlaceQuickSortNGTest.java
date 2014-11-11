f13fcf3245535aa150003aa44951469b897d2ad3
Marc Jan Bonder <bonder.m.j@gmail.com>
2014-11-11 11:21:03 +0100

    Tests for sorting

--- genetica-libraries/src/test/java/umcg/genetica/util/MultiThreadedInPlaceQuickSortNGTest.java
+++ genetica-libraries/src/test/java/umcg/genetica/util/MultiThreadedInPlaceQuickSortNGTest.java
@@ -1,23 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package umcg.genetica.util;
-
-import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Random;
-
-import static org.junit.Assert.assertArrayEquals;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 /**
- * Tests for the sorter.
  *
- * @author Kenneth Powers
+ * @author MarcJan
  */
-public class MultiThreadedInPlaceQuickSortTest {
+public class MultiThreadedInPlaceQuickSortNGTest {
+    
+    public MultiThreadedInPlaceQuickSortNGTest() {
+    }
+
     /**
-     * Tests the Sorter implementation on small set of values.
+     * Test of quicksort method, of class MultiThreadedInPlaceQuickSort.
      */
     @Test
-    public void testSort() {
+    public void testQuicksort() {
         // Define some values to sort.
         Integer[] values = {0, 9, 2, 5, 1, 5, 2, 7, 9, 2, 5, 0, 5, 4, 1};
         // Copy the values into a new array to sort with Java's built in implementation for comparison
@@ -28,17 +34,15 @@ public class MultiThreadedInPlaceQuickSortTest {
         // Multi-Threaded Quick Sort
         MultiThreadedInPlaceQuickSort.quicksort(values);
         // Assert equality.
-        assertArrayEquals(sortedValues, values);
+        assertEquals(sortedValues, values);
     }
-
-    /**
-     * Tests the Sorter implementation on a larger set of values.
-     */
+    
     @Test
-    public void largeTestSort() {
+    public void testlargeQuickSort() {
         // Generate an array of one million random integers.
         Random random = new Random(System.currentTimeMillis());
         Integer[] values = new Integer[10000000];
+        
         final long generateStart = System.currentTimeMillis();
         for (int i = 0; i < values.length; i++) {
             values[i] = random.nextInt();
@@ -47,26 +51,32 @@ public class MultiThreadedInPlaceQuickSortTest {
         System.out.printf("Generation time: %d (%d Integer Objects)%n", generateEnd - generateStart, values.length);
 
         // Copy the values into a new array to sort with Java's built in implementation for comparison
-        Integer[] sortedValues = new Integer[10000000];
+        Integer[] values2 = new Integer[10000000];
+        Integer[] values3 = new Integer[10000000];
         final long copyStart = System.currentTimeMillis();
-        System.arraycopy(values, 0, sortedValues, 0, values.length);
+        System.arraycopy(values, 0, values3, 0, values.length);
+        System.arraycopy(values, 0, values2, 0, values.length);
         final long copyEnd = System.currentTimeMillis();
         System.out.println("Copy time: " + (copyEnd - copyStart));
 
         // Start sorting
         final long sortStartTime = System.currentTimeMillis();
         // Java sort
-        Arrays.sort(sortedValues);
+        Arrays.sort(values3);
         final long sortTradeOffTime = System.currentTimeMillis();
         // Multi-Threaded Quick Sort
         MultiThreadedInPlaceQuickSort.quicksort(values);
+        final long sortTradeOffTime2 = System.currentTimeMillis();
+        // Multi-Threaded Quick Sort
+        InplaceArrayQuickSort.sort(values2);
         final long sortEndTime = System.currentTimeMillis();
-
         // Assert equality.
-        assertArrayEquals(sortedValues, values);
+        assertEquals(values3, values);
+        assertEquals(values2, values);
 
         // Print Statistics
         System.out.println("Java built in implementation: " + (sortTradeOffTime - sortStartTime));
-        System.out.println("Multi-threaded quick sort: " + (sortEndTime - sortTradeOffTime));
+        System.out.println("InplaceArrayQuickSort quick sort: " + (sortEndTime - sortTradeOffTime2));
+        System.out.println("Multi-threaded quick sort: " + (sortTradeOffTime2 - sortTradeOffTime));
     }
 }
