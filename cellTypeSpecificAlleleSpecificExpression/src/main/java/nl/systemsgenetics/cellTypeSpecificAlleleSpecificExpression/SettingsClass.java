86d81ebabc7fa0150079bd542bb7e45bd3e7714e
Adriaan van der Graaf <adriaan.vd.graaf@gmail.com>
2015-07-16 09:55:37 +0200

    Added a test for ASREADS and want to have a commit point before refractoring.

---
+++ cellTypeSpecificAlleleSpecificExpression/src/main/java/nl/systemsgenetics/cellTypeSpecificAlleleSpecificExpression/SettingsClass.java
@@ -0,0 +1,25 @@
+package nl.systemsgenetics.cellTypeSpecificAlleleSpecificExpression;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author adriaan
+ * Used as a class to store all variables in. 
+ * Needs to be implemented soon, but the code needs quite some refactoring  
+ * 
+ */
+public class SettingsClass {
+    
+    public static int minReads = 0;
+    public static int minHets  = 0;
+    
+    public static int maximumIterations = 20000;
+    public static double simplexPrecision  = 1e7;
+    
+    
+}
