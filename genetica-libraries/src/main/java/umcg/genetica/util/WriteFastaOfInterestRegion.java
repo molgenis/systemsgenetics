f588d36b03c241cf10933fd09b5ee3113025a792
Marc Jan Bonder <bonder.m.j@gmail.com>
2016-03-09 11:37:38 +0100

    Fix in naming and addition of bed

--- genetica-libraries/src/main/java/umcg/genetica/util/WriteFastaOfInterestRegion.java
+++ genetica-libraries/src/main/java/umcg/genetica/util/WriteFastaOfInterestRegion.java
@@ -22,20 +22,30 @@ import umcg.genetica.io.text.TextFile;
 public class WriteFastaOfInterestRegion {
 
     public static void main(String[] args) {
-            String referenceFasta = args[0];
-            String fileWithPositions = args[1];
-            String outputFile = args[2];
+        String referenceFasta = args[0];
+        String fileWithPositions = args[1];
+        String outputFile = args[2];
+        boolean modeBed = false;
+        if (args.length > 3) {
+            if (args[3].equals("bed")) {
+                modeBed = true;
+            }
+        }
 
 //        String referenceFasta = "E:\\OnlineFolders\\BitSync\\UMCG\\ProbeMapping\\human_g1k_v37.fasta";
 //        String fileWithPositions = "E:\\OnlineFolders\\BitSync\\UMCG\\ProbeMapping\\Info\\V71_2\\RNA_Seq\\IlluminaMatchedRNASeqProbes.gff";
 //        String outputFile = "E:\\OnlineFolders\\BitSync\\UMCG\\ProbeMapping\\Fasta_Urmo_1.txt";
-
         ReferenceGenomeFasta refGen = null;
         HashMap<String, HashMap<String, Triple<String, Integer, Integer>>> interestStrings = null;
 
         try {
-            System.out.println("Reading reference GTF.");
-            interestStrings = readFileWithPositions(fileWithPositions);
+            if(!modeBed){
+                System.out.println("Reading reference GTF.");
+                interestStrings = readFileWithPositions(fileWithPositions);
+            } else {
+                System.out.println("Reading reference Bed.");
+                interestStrings = readBedFileWithPositions(fileWithPositions);
+            }
         } catch (Exception ex) {
             Logger.getLogger(WriteFastaOfInterestRegion.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -72,19 +82,47 @@ public class WriteFastaOfInterestRegion {
             }
             int start = Integer.parseInt(parts[3]);
             int stop = Integer.parseInt(parts[4]);
-            if(stop<start){
-               int tmp = stop;
-               stop = start;
-               start = tmp;
+            if (stop < start) {
+                int tmp = stop;
+                stop = start;
+                start = tmp;
             }
-            chromosomalProbeInformation.get(parts[0]).put(parts[8]+"|_"+parts[6], new Triple<String, Integer, Integer>(parts[6], start, stop));
+            chromosomalProbeInformation.get(parts[0]).put(parts[8], new Triple<>(parts[6], start, stop));
             counter++;
         }
         System.out.println("Number of entries read in: " + counter);
         gffFileReader.close();
         return chromosomalProbeInformation;
     }
+    
+    private static HashMap<String, HashMap<String, Triple<String, Integer, Integer>>> readBedFileWithPositions(String fileWithPositions) throws IOException {
+        HashMap<String, HashMap<String, Triple<String, Integer, Integer>>> chromosomalProbeInformation = new HashMap<>();
+        TextFile gffFileReader = new TextFile(fileWithPositions, TextFile.R);
 
+        int counter = 0;
+        String str;
+        while ((str = gffFileReader.readLine()) != null) {
+            String[] parts = str.split("\t");
+//            System.out.println(str);
+            parts[0] = parts[0].replace("chr", "");
+            if (!chromosomalProbeInformation.containsKey(parts[0])) {
+                chromosomalProbeInformation.put(parts[0], new HashMap<String, Triple<String, Integer, Integer>>());
+            }
+            int start = Integer.parseInt(parts[1]);
+            int stop = Integer.parseInt(parts[2]);
+            if (stop < start) {
+                int tmp = stop;
+                stop = start;
+                start = tmp;
+            }
+            chromosomalProbeInformation.get(parts[0]).put(parts[3], new Triple<>(parts[5], start, stop));
+            counter++;
+        }
+        System.out.println("Number of entries read in: " + counter);
+        gffFileReader.close();
+        return chromosomalProbeInformation;
+    }
+    
     private static void writeFasta(ReferenceGenomeFasta refGen, HashMap<String, HashMap<String, Triple<String, Integer, Integer>>> interestStrings, String outputFile) throws IOException {
         TextFile writer = new TextFile(outputFile, TextFile.W);
 
@@ -98,16 +136,16 @@ public class WriteFastaOfInterestRegion {
                     s.append(">").append(probe.getKey()).append("\n");
                     String probeSeq = refGen.getNucleotides(probesPerChr.getKey(), probe.getValue().getMiddle(), probe.getValue().getRight()).toString();
 
-                    if (probe.getKey().endsWith("|_+")) {
+                    if (probe.getValue().getLeft().equals("+")) {
                         s.append(probeSeq).append("\n");
 //                        System.out.println(s.toString());
                         writer.write(s.toString());
-                    } else if (probe.getKey().endsWith("|_-")) {
+                    } else if (probe.getValue().getLeft().equals("-")) {
                         s.append(WriteExtendedFastaOfInterestRegion.getFullComplement(WriteExtendedFastaOfInterestRegion.reverse(probeSeq))).append("\n");
 //                        System.out.println(s.toString());
                         writer.write(s.toString());
                     } else {
-                        System.out.println("Error with the directionality of the probe: "+ probe.getKey());
+                        System.out.println("Error with the directionality of the probe: " + probe.getKey());
                     }
 
                 } catch (Exception ex) {
