d34c5cac18a5449b5bc0a323a8842033ff38053e
Marc Jan Bonder <bonder.m.j@gmail.com>
2016-05-03 13:49:27 +0200

    ups

--- genetica-libraries/src/main/java/umcg/genetica/util/WriteExtendedFastaOfInterestRegion.java
+++ genetica-libraries/src/main/java/umcg/genetica/util/WriteExtendedFastaOfInterestRegion.java
@@ -27,29 +27,43 @@ public class WriteExtendedFastaOfInterestRegion {
 //            String outputFile = args[2];
 
         String referenceFasta = "E:\\OnlineFolders\\BitSync\\UMCG\\ProbeMapping\\human_g1k_v37.fasta";
-        String fileWithPositions = "E:\\OnlineFolders\\BitSync\\UMCG\\ProbeMapping\\Info\\V71_2\\RNA_Seq\\IlluminaMatchedRNASeqProbes.gff";
-        String outputFile = "E:\\OnlineFolders\\BitSync\\UMCG\\ProbeMapping\\Fasta_Urmo_1.txt";
-
+        
+//        String fileWithPositions = "E:\\OnlineFolders\\BitSync\\UMCG\\ProbeMapping\\Info\\V71_2\\RNA_Seq\\IlluminaMatchedRNASeqProbes.gff";
+//        String outputFile = "E:\\OnlineFolders\\BitSync\\UMCG\\ProbeMapping\\Fasta_Urmo_1.txt";
+//        boolean bedFile = false;
+        
+        String fileWithPositions = "E:\\OnlineFolders\\AeroFS\\RP3_BIOS_Methylation\\Annotations\\Illumina450K_MQtlMappingFile_MJB.txt";
+        String outputFile = "E:\\OnlineFolders\\BitSync\\UMCG\\Fastatmp.txt";
+        boolean bedFile = true;
+        
+        int maxScore = 5;
+        int minDiff = 30;
+        
         ReferenceGenomeFasta refGen = null;
         HashMap<String, HashMap<String, Triple<String, Integer, Integer>>> interestStrings = null;
 
         try {
-            System.out.println("Reading reference GTF.");
-            interestStrings = readFileWithPositions(fileWithPositions);
+            if(!bedFile){
+                System.out.print("Reading reference GTF ...... ");
+                interestStrings = readFileWithPositions(fileWithPositions);
+            } else {
+                System.out.println("Reading reference BED .....");
+                interestStrings = readFileWithPositionsBed(fileWithPositions);
+            }
         } catch (Exception ex) {
             Logger.getLogger(WriteExtendedFastaOfInterestRegion.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        System.out.println(" done");
         try {
-            System.out.println("Reading reference FASTA.");
+            System.out.print("Reading reference FASTA ......");
             refGen = new ReferenceGenomeFasta(new File(referenceFasta), ReferenceGenomeFasta.HUMAN_NORMAL_CHR);
         } catch (Exception ex) {
             Logger.getLogger(WriteExtendedFastaOfInterestRegion.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        System.out.println(" done");
         if (refGen != null && interestStrings != null) {
             try {
-                writeFastaIncDirectionGuessing(refGen, interestStrings, outputFile);
+                writeFastaIncDirectionGuessing(refGen, interestStrings, outputFile, maxScore, minDiff);
             } catch (IOException ex) {
                 Logger.getLogger(WriteExtendedFastaOfInterestRegion.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -78,9 +92,32 @@ public class WriteExtendedFastaOfInterestRegion {
         gffFileReader.close();
         return chromosomalProbeInformation;
     }
+    
+    private static HashMap<String, HashMap<String, Triple<String, Integer, Integer>>> readFileWithPositionsBed(String fileWithPositions) throws IOException {
+        HashMap<String, HashMap<String, Triple<String, Integer, Integer>>> chromosomalProbeInformation = new HashMap<>();
+        TextFile gffFileReader = new TextFile(fileWithPositions, TextFile.R);
 
-    private static void writeFastaIncDirectionGuessing(ReferenceGenomeFasta refGen, HashMap<String, HashMap<String, Triple<String, Integer, Integer>>> interestStrings, String outputFile) throws IOException {
+        int counter = 0;
+        String str = gffFileReader.readLine();
+        while ((str = gffFileReader.readLine()) != null) {
+            String[] parts = str.split("\t");
+//            System.out.println(str);
+
+            if (!chromosomalProbeInformation.containsKey(parts[3])) {
+                chromosomalProbeInformation.put(parts[3], new HashMap<String, Triple<String, Integer, Integer>>());
+            }
+//            //\"; IlluminaProbeSeq \" + parts[7]
+            chromosomalProbeInformation.get(parts[3]).put(parts[1]+"\"; IlluminaProbeSeq \"" + parts[7], new Triple<String, Integer, Integer>(".", Integer.parseInt(parts[4]), Integer.parseInt(parts[5])));
+            counter++;
+        }
+        System.out.println("Number of entries read in: " + counter);
+        gffFileReader.close();
+        return chromosomalProbeInformation;
+    }
+
+    private static void writeFastaIncDirectionGuessing(ReferenceGenomeFasta refGen, HashMap<String, HashMap<String, Triple<String, Integer, Integer>>> interestStrings, String outputFile, int maxScore, int minDiff) throws IOException {
         TextFile writer = new TextFile(outputFile, TextFile.W);
+        
 
         for (Entry<String, HashMap<String, Triple<String, Integer, Integer>>> probesPerChr : interestStrings.entrySet()) {
 //            System.out.println(probesPerChr.getKey());
@@ -88,22 +125,23 @@ public class WriteExtendedFastaOfInterestRegion {
             for (Entry<String, Triple<String, Integer, Integer>> probe : probesPerChr.getValue().entrySet()) {
 //                System.out.println("\t"+probe.getKey() + "      "+ probe.getValue().getLeft());
                 try {
+                    String[] partsKey = probe.getKey().split("\";");
                     StringBuilder s = new StringBuilder();
-                    s.append(">").append(probe.getKey()).append("\t");
+                    s.append(partsKey[0]).append("\t");
                     String probeSeq = refGen.getNucleotides(probesPerChr.getKey(), probe.getValue().getMiddle(), probe.getValue().getRight()).toString();
 
                     if (probe.getKey().contains(probeSeq)) {
                         s.append("+").append("\n");
-                        s.append(probeSeq).append("\n");
+//                        s.append(probeSeq).append("\n");
 //                        System.out.println(s.toString());
                         writer.write(s.toString());
                     } else if (probe.getKey().contains(getFullComplement(reverse(probeSeq)))) {
                         s.append("-").append("\n");
-                        s.append(getFullComplement(reverse(probeSeq))).append("\n");
+//                        s.append(getFullComplement(reverse(probeSeq))).append("\n");
 //                        System.out.println(s.toString());
                         writer.write(s.toString());
                     } else {
-                        String[] partsKey = probe.getKey().split("\";");
+                        
                         partsKey[1] = partsKey[1].replace(" IlluminaProbeSeq \"", "");
                         if (partsKey[1].length() == probeSeq.length()) {
 //                        System.out.println(partsKey[1]);
@@ -113,17 +151,17 @@ public class WriteExtendedFastaOfInterestRegion {
                             if (editDistancePos < 15 || editDistanceNeg < 15 ||  Math.abs(editDistancePos-editDistanceNeg)>10) {
                                 if (editDistancePos > editDistanceNeg) {
                                     s.append("-").append("\n");
-                                    s.append(getFullComplement(reverse(probeSeq))).append("\n");
+//                                    s.append(getFullComplement(reverse(probeSeq))).append("\n");
                                     //                        System.out.println(s.toString());
                                     writer.write(s.toString());
                                 } else {
                                     s.append("+").append("\n");
-                                    s.append(probeSeq).append("\n");
+//                                    s.append(probeSeq).append("\n");
                                     //                        System.out.println(s.toString());
                                     writer.write(s.toString());
                                 }
                             } else {
-                                s.append("?").append("\n");
+                                s.append(partsKey[1]).append("\t?\n");
                                 s.append("").append("\n");
                                 System.out.println(s.toString());
                                 System.out.println("editDistance: "+  editDistancePos + "\t"+ probeSeq);
