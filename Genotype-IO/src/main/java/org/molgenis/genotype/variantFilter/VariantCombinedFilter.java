36767e0f809b63e7b4c776cd06523a002804d693
Marc Jan Bonder <bonder.m.j@gmail.com>
2017-05-11 13:03:22 +0100

    Addition of Ambigous SNP filter for Genotype Harmonizer

--- Genotype-IO/src/main/java/org/molgenis/genotype/variantFilter/VariantCombinedFilter.java
+++ Genotype-IO/src/main/java/org/molgenis/genotype/variantFilter/VariantCombinedFilter.java
@@ -16,58 +16,55 @@ import org.molgenis.genotype.variant.GeneticVariant;
  */
 public class VariantCombinedFilter implements VariantFilter {
 
-	private final List<VariantFilter> variantFilters;
+    private final List<VariantFilter> variantFilters;
 
-	/**
-	 * Allows specification of arbitrary number of variant filters
-	 *
-	 * Example: new VariantCombinedFilter(new VariantQcChecker(0.05, 0.95,
-	 * 0.005), new VariantIdIncludeFilter(null));
-	 *
-	 * @param variantFilters
-	 */
-    
-	public VariantCombinedFilter(VariantFilter... variantFilters) {
-		this.variantFilters = Arrays.asList(variantFilters);
-	}
-    
+    /**
+     * Allows specification of arbitrary number of variant filters
+     *
+     * Example: new VariantCombinedFilter(new VariantQcChecker(0.05, 0.95,
+     * 0.005), new VariantIdIncludeFilter(null));
+     *
+     * @param variantFilters
+     */
+    public VariantCombinedFilter(VariantFilter... variantFilters) {
+        this.variantFilters = Arrays.asList(variantFilters);
+    }
+
     public VariantCombinedFilter() {
-		this.variantFilters = new ArrayList<VariantFilter>();
-	}
+        this.variantFilters = new ArrayList<VariantFilter>();
+    }
 
-	public VariantCombinedFilter(List<VariantFilter> variantFilters) {
-		this.variantFilters = variantFilters == null ? new ArrayList<VariantFilter>() : variantFilters;
-	}
+    public VariantCombinedFilter(List<VariantFilter> variantFilters) {
+        this.variantFilters = variantFilters == null ? new ArrayList<VariantFilter>() : variantFilters;
+    }
 
-	@Override
-	public boolean doesVariantPassFilter(GeneticVariant variant) {
+    @Override
+    public boolean doesVariantPassFilter(GeneticVariant variant) {
 
-		for (VariantFilter filter : variantFilters) {
-            if(!filter.doesVariantPassFilter(variant)) {
-				return false;
-			}
-		}
-		return true;
+        for (VariantFilter filter : variantFilters) {
+            if (!filter.doesVariantPassFilter(variant)) {
+                return false;
+            }
+        }
+        return true;
 
+    }
 
-	}
+    @Override
+    public boolean doesIdPassFilter(String id) {
+        for (VariantFilter filter : variantFilters) {
+            if (!filter.doesIdPassFilter(id)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	@Override
-	public boolean doesIdPassFilter(String id) {
-		for (VariantFilter filter : variantFilters) {
-			if (!filter.doesIdPassFilter(id)) {
-				return false;
-			}
-		}
-		return true;
-	}
-    
-    public void add(VariantFilter v){
-		if(v == this){
-			throw new GenotypeDataException("Can not add a variant filter to it self");
-		}
+    public void add(VariantFilter v) {
+        if (v == this) {
+            throw new GenotypeDataException("Can not add a variant filter to it self");
+        }
         variantFilters.add(v);
     }
-	
-	
+
 }
