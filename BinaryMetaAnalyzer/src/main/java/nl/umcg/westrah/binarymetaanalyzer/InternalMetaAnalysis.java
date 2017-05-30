b332bc64e203b98ca41a5b7d875203930cec9b5c
Marc Jan Bonder <bonder.m.j@gmail.com>
2017-05-30 16:01:28 +0100

    Internal Meta analysis progress

--- BinaryMetaAnalyzer/src/main/java/nl/umcg/westrah/binarymetaanalyzer/InternalMetaAnalysis.java
+++ BinaryMetaAnalyzer/src/main/java/nl/umcg/westrah/binarymetaanalyzer/InternalMetaAnalysis.java
@@ -32,8 +32,8 @@ public class InternalMetaAnalysis {
     private final InternalMetaAnalysisSettings settings;
 
     HashMap<String, String> traitMap = new HashMap<String, String>();
-    LinkedHashMap<String,Integer> outputEntries = null;
-    
+    LinkedHashMap<String, Integer> outputEntries = null;
+
     public static void main(String[] args) {
 
         String settingsFile = null;
@@ -55,8 +55,6 @@ public class InternalMetaAnalysis {
         }
 
         InternalMetaAnalysis meta = new InternalMetaAnalysis(settingsFile, textToReplace, replaceTextWith);
-        System.exit(0);
-
     }
 
     public InternalMetaAnalysis(String settingsFile, String textToReplace, String replaceTextWith) {
@@ -76,7 +74,7 @@ public class InternalMetaAnalysis {
 
         String outdir = settings.getOutput();
 
-        outputEntries = new LinkedHashMap<String,Integer>();
+        outputEntries = new LinkedHashMap<String, Integer>();
         traitMap = new HashMap<String, String>();
 
         TextFile entryReader = new TextFile(settings.getProbeToFeature(), TextFile.R);
@@ -85,15 +83,14 @@ public class InternalMetaAnalysis {
         while ((row = entryReader.readLine()) != null) {
 //            System.out.println(row);
             String[] parts = TAB_PATTERN.split(row);
-            
-            if(!outputEntries.containsKey(parts[1])){
+
+            if (!outputEntries.containsKey(parts[1])) {
                 outputEntries.put(parts[1], index);
                 index++;
             }
-            
+
             traitMap.put(parts[0], parts[1]);
         }
-
 
         for (int permutation = settings.getStartPermutations(); permutation <= settings.getNrPermutations(); permutation++) {
             boolean runningPermutation = true;
@@ -140,12 +137,91 @@ public class InternalMetaAnalysis {
             tf.writeList(new ArrayList<String>(outputEntries.keySet()));
             tf.close();
 
-            //Writing from original result processing
-            //Get data from a thread and write R.
-            //writeBinaryResult(r);
-            //meta-analyze and write output
+            // create dataset objects
+            // run analysis
+            System.out.println("Starting meta-analysis");
+            ProgressBar pb = new ProgressBar(snpList.length);
+            for (int snp = 0; snp < snpList.length; snp++) {
+                //Here now. Need to check what probes to meta-analyze, do the meta-analysis and write.
+
+                // do we need to check if alleles are different in the same dataset?
+                // get ZScores for this SNP, no matter what.
+                // get list of probes to test
+                // do cis stuff
+                // get all the possible traits for SNP
+                HashMap<String, Integer> probeMap = new HashMap<String, Integer>();
+                float[] finalZScores = new float[probeMap.size()]; // size: [possible cis-probes][nr of datasets]
+
+                // get list of probes to test for each dataset
+                //initialize z-score
+                for (int p = 0; p < probeMap.size(); p++) {
+                    finalZScores[p] = Float.NaN; // this is not very nice, but does prevent the metaZ method from going nuts
+                }
+                // load the z-scores for the dataset
+                int datasetSNPId = snpIndex[snp];
+
+                if (datasetSNPId != -9) { // -9 means: snp not available
+                    float[] datasetZScores = dataset.getZScores(datasetSNPId);
+                    
+                    System.out.println(datasetZScores.length);
+                    if (dataset.getIsCisDataset()) {
+                        // this requires us to retrieve the z-scores differently
+                        // we need to figure out which probes match up, but their orders might be different
+                        // and the number of probes tested in each dataset might differ as well
+
+                        // get the probes tested against the SNP
+                        MetaQTL4MetaTrait[] datasetCisProbes = dataset.getCisProbes(datasetSNPId);
+                        System.out.println(datasetCisProbes.length);
+                        
+//                        for (int i = 0; i < datasetCisProbes.length; i++) {
+//                            MetaQTL4MetaTrait p = datasetCisProbes[i];
+//                            if (p != null) {
+//                                Integer index = cisProbeMap.get(p);
+//                                if (index != null) {
+//                                    float datasetZ = datasetZScores[i];
+//                                    finalZScores[index] = datasetZ;
+//                                }
+//                            }
+//                        }
+
+                    } else { // this is not a cis dataset
+                        // use the full probe index
+//                        for (int probe = 0; probe < cisProbeArray.length; probe++) {
+//                            MetaQTL4MetaTrait cisProbe = cisProbeArray[probe];
+//                            Integer metaProbeIndex = traitMap.get(cisProbe);
+//                            Integer datasetProbeId = probeIndex[metaProbeIndex][d];
+//                            if (datasetProbeId != null) {
+//                                finalZScores[probe][d] = datasetZScores[datasetProbeId];
+//                                if (flipZScores[d]) {
+//                                    finalZScores[probe][d] *= -1;
+//                                }
+//                            }
+//                        }
+                    }
+                }
+
+                //Writing from original result processing
+                //Get data from a thread and write R.
+                //writeBinaryResult(r);
+                //meta-analyze and write output
+//                for (int probe = 0; probe < finalZScores.length; probe++) {
+//                    MetaQTL4MetaTrait t = cisProbeArray[probe];
+//
+//                    double metaZ = ZScores.getWeightedZ(finalZScores[probe], sampleSizes);
+//                    double p = Descriptives.convertZscoreToPvalue(metaZ);
+//
+//                    if (!Double.isNaN(p) && !Double.isNaN(metaZ)) {
+//                        // create output object
+//                        QTL q = new QTL(p, t, snp, BaseAnnot.toByte(alleleAssessed), metaZ, BaseAnnot.toByteArray(alleles), finalZScores[probe], sampleSizes); // sort buffer if needed.
+////                            System.out.println(q.getSNPId()+"\t"+q.getMetaTrait().getMetaTraitName()+"\t"+q.toString());
+//                        addEQTL(q);
+//                    }
+//                }
+                pb.iterate();
+            }
+            pb.close();
+
             //Close binaryOutput & Create binary check file.
-            //To move later!
             zScoreBinaryFile.close();
             fileName = "check";
             if (runningPermutation) {
@@ -164,73 +240,7 @@ public class InternalMetaAnalysis {
             md5writer.write(md5Parser.marshal(zScoreBinaryFile.getWrittenHash()) + "  " + fileName + '\n');
             zScoreRowNamesFile.close();
             md5writer.close();
-            //
 
-            // create dataset objects
-            // run analysis
-
-            System.out.println("Starting meta-analysis");
-            ProgressBar pb = new ProgressBar(snpList.length);
-            for (int snp = 0; snp < snpList.length; snp++) {
-//                //Here now. Need to check what probes to meta-analyze, do the meta-analysis and write.
-//
-//                // do we need to check if alleles are different in the same dataset?
-//
-//                // get ZScores for this SNP, no matter what.
-//                // get list of probes to test
-//                // do cis stuff
-//
-//                // get all the possible traits near the SNP
-//                HashMap<String, Integer> probeMap = new HashMap<String, Integer>();
-//
-//
-//                float[] finalZScores = new float[probeMap.size()]; // size: [possible cis-probes][nr of datasets]
-//
-//                // get list of probes to test for each dataset
-//
-//                //initialize z-score
-//                for (int p = 0; p < probeMap.size(); p++) {
-//                    finalZScores[p] = Float.NaN; // this is not very nice, but does prevent the metaZ method from going nuts
-//                }
-//                // load the z-scores for the dataset
-//                int datasetSNPId = snpIndex[snp];
-//
-//                if (datasetSNPId != -9) { // -9 means: snp not available
-//                    float[] datasetZScores = dataset.getZScores(datasetSNPId);
-//                    
-//                    // use the full probe index
-//                    for (int probe = 0; probe < cisProbeArray.length; probe++) {
-//                        MetaQTL4MetaTrait cisProbe = cisProbeArray[probe];
-//                        Integer metaProbeIndex = traitMap.get(cisProbe);
-//                        Integer datasetProbeId = probeIndex[metaProbeIndex][d];
-//                        if (datasetProbeId != null) {
-//                            finalZScores[probe][d] = datasetZScores[datasetProbeId];
-//                            if (flipZScores[d]) {
-//                                finalZScores[probe][d] *= -1;
-//                            }
-//                        }
-//                    }
-//                }
-//
-//                for (int probe = 0; probe < finalZScores.length; probe++) {
-//                    MetaQTL4MetaTrait t = cisProbeArray[probe];
-//
-//                    double metaZ = ZScores.getWeightedZ(finalZScores[probe], sampleSizes);
-//                    double p = Descriptives.convertZscoreToPvalue(metaZ);
-//
-//                    if (!Double.isNaN(p) && !Double.isNaN(metaZ)) {
-//                        // create output object
-//                        QTL q = new QTL(p, t, snp, BaseAnnot.toByte(alleleAssessed), metaZ, BaseAnnot.toByteArray(alleles), finalZScores[probe], sampleSizes); // sort buffer if needed.
-////                            System.out.println(q.getSNPId()+"\t"+q.getMetaTrait().getMetaTraitName()+"\t"+q.toString());
-//                        addEQTL(q);
-//                    }
-//                }
-                pb.iterate();
-            }
-            pb.close();
-
-
-//            writeBuffer(outdir, permutation);
         }
     }
 
@@ -258,7 +268,7 @@ public class InternalMetaAnalysis {
         for (int s = 0; s < allSNPs.size(); s++) {
             snpIndex[s] = -9;
         }
-        
+
         for (int s = 0; s < snps.length; s++) {
             String snp = snps[s];
             int id = snpMap.get(snp);
@@ -271,7 +281,7 @@ public class InternalMetaAnalysis {
         String header = "metaID";
         header += "\t" + dataset.getName() + "-sid";
         tf.writeln(header);
-        
+
         for (int s = 0; s < snpList.length; s++) {
             String ln = snpList[s];
             ln += "\t" + snpIndex[s];
@@ -296,10 +306,10 @@ public class InternalMetaAnalysis {
 //            System.out.println(t);
             Integer index = outputEntries.get(t);
 
-            if (index!=null && confineToTheseProbes.contains(probes[p])) {
+            if (index != null && confineToTheseProbes.contains(probes[p])) {
                 probeIndex[p] = index;
             } else {
-                probeIndex[p]=null;
+                probeIndex[p] = null;
             }
         }
 
