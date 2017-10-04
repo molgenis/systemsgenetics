fe59adcf85002508f2c59e1c15b44132ace3d0cd
npklein <niekdeklein@gmail.com>
2017-10-04 12:30:54 +0200

    final rename

--- deconvolution/testrun.sh
+++
@@ -1,8 +0,0 @@
-java -Xms8000M -Xmx10000M -jar deconvolution.jar \
-    -o /Users/NPK/UMCG/tmp/decon/ \
-    -c /Users/NPK/UMCG/tmp/decon/simulatedCellcount.txt \
-    -e /Users/NPK/UMCG/tmp/decon/simulatedExpressionData.txt \
-    -sn /Users/NPK/UMCG/tmp/decon/simulatedGeneSNPpair.txt \
-    -g /Users/NPK/UMCG/tmp/decon/simulatedGenotypeData.txt \
-    --output_coefficients \
-    --whole_blood_qtl 
