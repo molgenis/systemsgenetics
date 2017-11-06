b982e4e50058ea9aa2df3af0235a1970f27bd155
Marc Jan Bonder <bonder.m.j@gmail.com>
2017-11-06 16:20:55 +0100

    Updates to bgen reader including test

--- Genotype-IO/src/main/java/org/molgenis/genotype/bgen/BgenGenotypeData.java
+++ Genotype-IO/src/main/java/org/molgenis/genotype/bgen/BgenGenotypeData.java
@@ -9,11 +9,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
 import org.apache.log4j.Logger;
 import org.molgenis.genotype.GenotypeDataException;
-import org.molgenis.genotype.variant.range.GeneticVariantRange;
 
 /**
  *
@@ -21,14 +22,24 @@ import org.molgenis.genotype.variant.range.GeneticVariantRange;
  */
 public class BgenGenotypeData {
 
+	public enum blockRepresentation {
+		compression_0, compression_1, compression_2
+	}
+
+	public enum layout {
+		layOut_1, layOut_2
+	}
 	private static final double DEFAULT_MINIMUM_POSTERIOR_PROBABILITY_TO_CALL = 0.4f;
 	private final RandomAccessFile bgenFile;
 	private final byte[] byteArray4 = new byte[4]; //resuable 4 byte array
+	private final byte[] byteArray2 = new byte[2]; //resuable 2 byte array
 	private static final Logger LOGGER = Logger.getLogger(BgenGenotypeData.class);
-	private final boolean compressedGenotypes;
-	private final boolean v1_1;
-	private final Inflater inflater = new Inflater();
-	private static final Charset charset = Charset.forName("UTF-8") ;
+	private final boolean sampleIdentifiersPresent;
+	private final Inflater gzipInflater = new Inflater();
+	private final Inflater gzipInflater = new Inflater();
+	private static final Charset charset = Charset.forName("UTF-8");
+	private final blockRepresentation snpBlockRepresentation;
+	private final layout fileLayout;
 
 	public BgenGenotypeData(File bgenFile, File sampleFile) throws IOException {
 		this(bgenFile, sampleFile, 1000);
@@ -53,151 +64,156 @@ public class BgenGenotypeData {
 			throw new GenotypeDataException("Error reading bgen file header. File is corrupt");
 		}
 		long headerSize = getUInt32(byteArray4, 0);
-		LOGGER.debug("Header size: " + headerSize);
-
+		LOGGER.debug("Header size (Lh): " + headerSize);
+		if(headerSize>snpOffset){
+			throw new GenotypeDataException("Error reading bgen file header. Header information is bigger than expected offset.");
+		}
+		//add checks for header size and header size must be smaller than offset.
 		if (this.bgenFile.read(byteArray4, 0, 4) != 4) {
 			throw new GenotypeDataException("Error reading bgen file header. File is corrupt");
 		}
-		long snpCount = getUInt32(byteArray4, 0);
-		LOGGER.debug("Number of SNPs: " + snpCount);
+		long variantCount = getUInt32(byteArray4, 0);
+		LOGGER.debug("Number of SNPs: " + variantCount);
 
-		if (snpCount > Integer.MAX_VALUE) {
-			throw new GenotypeDataException("Found more than (2^31)-1 SNPs in bgen file. This is not supported");
-		}
+//               Not needed when using the bgenix indexing file.  
+//		if (snpCount > Integer.MAX_VALUE) {
+//			throw new GenotypeDataException("Found more than (2^31)-1 SNPs in bgen file. This is not supported");
+//		}
 
 		if (this.bgenFile.read(byteArray4, 0, 4) != 4) {
 			throw new GenotypeDataException("Error reading bgen file header. File is corrupt");
 		}
 		long sampleCount = getUInt32(byteArray4, 0);
 		LOGGER.debug("Number of samples: " + sampleCount);
+		
+		if (sampleCount > Integer.MAX_VALUE) {
+			throw new GenotypeDataException("Found more than (2^31)-1 samples in bgen file. This is not supported");
+		}
+		//Magic number is in the next four bytes but skipped over.
 
-		//Skip over reserved and free area
+		//Skip over reserved and free area, 
+		//this seek works because there are 4 (interesting) bytes 
+		//in the beginning of the file and in the end of the header block.
 		this.bgenFile.seek(headerSize);
 
 		//Read flags
 		if (this.bgenFile.read(byteArray4, 0, 4) != 4) {
 			throw new GenotypeDataException("Error reading bgen file header. File is corrupt");
 		}
-		if ((byteArray4[0] & 1) == 1) {
-			LOGGER.debug("Genotype data is compressed");
-			compressedGenotypes = true;
-		} else {
-			LOGGER.debug("Genotype data is not compressed");
-			compressedGenotypes = false;
+
+		//Changed storing of version and data.
+		switch (byteArray4[0] & 3) {
+			case 0:
+				LOGGER.debug("Genotype data is not compressed.");
+				snpBlockRepresentation = blockRepresentation.compression_0;
+				break;
+			case 1:
+				LOGGER.debug("Genotype data is zlib compressed");
+				snpBlockRepresentation = blockRepresentation.compression_1;
+				break;
+			case 2:
+				LOGGER.debug("Genotype data is zstd compressed");
+				snpBlockRepresentation = blockRepresentation.compression_2;
+				break;
+			default:
+				throw new GenotypeDataException("Invalid compression method, observed: " + (byteArray4[0] & 3));
 		}
-
-		if ((byteArray4[0] & 3) == 1) {
-			LOGGER.debug("v1.1 gen file");
-			v1_1 = true;
-		} else {
-			LOGGER.debug("v1.0 gen file");
-			v1_1 = false;
-			throw new GenotypeDataException("bgen version v1.0 is not supported at the moment. Feel free to contact the developers of Genotype IO");
+		byte byte_tmp =  byteArray4[0];
+		byte_tmp = (byte) (byte_tmp >> 2);
+		switch (byte_tmp & 7) {
+			case 1:
+				LOGGER.debug("Genotype data is in layout 1 (BGEN 1.1)");
+				fileLayout = layout.layOut_1;
+				break;
+			case 2:
+				LOGGER.debug("Genotype data is in layout 2 (BGEN 1.2 & 1.3)");
+				fileLayout = layout.layOut_2;
+				break;
+			default:
+				throw new GenotypeDataException("Invalid layout, observed: " + (byteArray4[0] & 3));
 		}
-
-		GeneticVariantRange.createRangeFactory((int) snpCount);
 		
-		byte[] snpInfoBuffer = new byte[8096];
-		this.bgenFile.seek(snpOffset + 4);
-		long lastSnpStart = snpOffset + 4;
-
-		for (int snpI = 0; snpI < snpCount; ++snpI) {
+		if(snpBlockRepresentation.equals(blockRepresentation.compression_2) & fileLayout.equals(layout.layOut_1)){
+			throw new GenotypeDataException("Invalid compression method for layout one observed. Trying to use ZSTD compression on layout one file, which is not supported.");
+		}
 		
-			int snpInfoBufferSize = this.bgenFile.read(snpInfoBuffer, 0, snpInfoBuffer.length);
-			int snpInfoBufferPos = 0;
+		if ((byteArray4[3] & 128) == 128) {
+			LOGGER.debug("SampleIdentifiers present in bgen file");
+			sampleIdentifiersPresent = true;
+		} else {
+			LOGGER.debug("SampleIdentifiers not present in bgen file");
+			sampleIdentifiersPresent = false;
+		}
 		
-			if (snpInfoBufferSize < 20) {
-				throw new GenotypeDataException("Error reading bgen snp data. File is corrupt");
+		
+		if(sampleIdentifiersPresent){
+			// Proccess sample indentifier block
+			if (this.bgenFile.read(byteArray4, 0, 4) != 4) {
+				throw new GenotypeDataException("Error in sample identifier block. File is corrupt.");
+			}
+			long byteSizeSampleIds = getUInt32(byteArray4, 0);
+			LOGGER.debug("LSI size: " + byteSizeSampleIds);
+			if((byteSizeSampleIds+headerSize)>snpOffset){
+				throw new GenotypeDataException("Error reading bgen file header. Combination of header & sample id information is bigger than expected offset.");
 			}
 			
-			long sampleCountSnp = getUInt32(snpInfoBuffer, snpInfoBufferPos);
-			System.out.println("test " + sampleCountSnp);
-			snpInfoBufferPos +=4;
-			
-			if(sampleCountSnp != sampleCount){
-				throw new GenotypeDataException("Error reading bgen variant data. General sample count is not the same as current variant sample count");
+			// Proccess sample indentifier block
+			if (this.bgenFile.read(byteArray4, 0, 4) != 4) {
+				throw new GenotypeDataException("Error in sample identifier block. File is corrupt.");
 			}
-			
-			
-			int fieldLength = getUInt16(snpInfoBuffer ,snpInfoBufferPos);
-			LOGGER.debug("Snp ID length " + fieldLength);
-			this.bgenFile.skipBytes(fieldLength);
-			
-			snpInfoBufferPos += 2 + fieldLength; // skip id length and snp id
-			
-			fieldLength = getUInt16(snpInfoBuffer ,snpInfoBufferPos);
-			LOGGER.debug("Snp RS length " + fieldLength);
-			
-			snpInfoBufferPos += 2;
-			String snpId = new String(snpInfoBuffer, snpInfoBufferPos, fieldLength, charset);
-			System.out.println(snpId);
-			
-			snpInfoBufferPos += fieldLength;
-			
-			fieldLength = getUInt16(snpInfoBuffer ,snpInfoBufferPos);
-			snpInfoBufferPos += 2;
-			String seqName = new String(snpInfoBuffer, snpInfoBufferPos, fieldLength, charset);
-			System.out.println(seqName);
-			snpInfoBufferPos += fieldLength;
-			
-			long snpPosLong = getUInt32(snpInfoBuffer, snpInfoBufferPos);
-			snpInfoBufferPos += 4;
-			if(snpPosLong > Integer.MAX_VALUE){
-				throw new GenotypeDataException("SNP pos larger than (2^31)-1 not supported");
+			long  sampleCount2 = getUInt32(byteArray4, 0);
+			LOGGER.debug("Number of samples with sample-ids: " + sampleCount2);
+			if(sampleCount2!=sampleCount){
+				throw new GenotypeDataException("Numer of samples in metadata and sample id data is not equal. File is corrupt.");
 			}
-			
-			int snpPos = (int) snpPosLong;
-			
-			System.out.println("SNP pos " + snpPos);
-			
-			long fieldLengthLong = getUInt32(snpInfoBuffer, snpInfoBufferPos);
-			snpInfoBufferPos += 4;
-			if(fieldLengthLong > Integer.MAX_VALUE){
-				throw new GenotypeDataException("SNP with allele longer than (2^31)-1 characters not supported");
+			String[] sampleIds = new String[(int)sampleCount2];
+			for(int i = 0; i<sampleCount2;i++){
+				if (this.bgenFile.read(byteArray2, 0, 2) != 2) {
+					throw new GenotypeDataException("Error in sample Id. File is corrupt.");
+				}
+				int sampleIdLength = getUInt16(byteArray2, 0);
+				byte[] sampleName = new byte[sampleIdLength];
+				this.bgenFile.read(sampleName, 0, sampleIdLength);
+				sampleIds[i] = new String(sampleName,charset);
+				System.out.println("\t"+sampleIds[i]);
 			}
-			String a1 = new String(snpInfoBuffer, snpInfoBufferPos, (int)fieldLengthLong, charset);
-			snpInfoBufferPos += ((int) fieldLengthLong);
-			
-			fieldLengthLong = getUInt32(snpInfoBuffer, snpInfoBufferPos);
-			snpInfoBufferPos += 4;
-			if(fieldLengthLong > Integer.MAX_VALUE){
-				throw new GenotypeDataException("SNP with allele longer than (2^31)-1 characters not supported");
-			}
-			String a2 = new String(snpInfoBuffer, snpInfoBufferPos, (int)fieldLengthLong, charset);
-			snpInfoBufferPos += ((int) fieldLengthLong);
-			
-			System.out.println(a1 + "/" + a2);
-			
-			long snpBlockSize;
-			if(compressedGenotypes){
-				snpBlockSize = getUInt32(snpInfoBuffer, snpInfoBufferPos) + 4; 
-			} else {
-				snpBlockSize = 6 * sampleCount;
-			}
-			
-			byte[] snpBlockData = new byte[6 * (int) sampleCount];
-			inflater.setInput(snpInfoBuffer, snpInfoBufferPos + 4, (int) snpBlockSize - 4);
-			try {
-				inflater.inflate(snpBlockData);
-			} catch (DataFormatException ex) {
-				throw new GenotypeDataException("Error decompressing bgen data", ex);
-			}
-			inflater.reset();
-			System.out.println(getUInt16(snpBlockData, 0) / 32768f + " " + getUInt16(snpBlockData, 2) / 32768f + " " + getUInt16(snpBlockData, 4) / 32768f );
-			
-			//move to next SNP
-			lastSnpStart = lastSnpStart + snpInfoBufferPos + snpBlockSize;
-			this.bgenFile.seek(lastSnpStart);
+		}
+//		System.out.println("offset:"+ (snpOffset + 4));
+		//Check if bgenix file is present.
+		File bgenix = new File(bgenFile.getAbsolutePath()+".bgi");
+		System.out.println(bgenFile.getAbsolutePath()+".bgi");
+		if(bgenix.exists()){
+			BgenixReader indexInformation = new BgenixReader(bgenix);
+			BgenixMetadata metadata = indexInformation.getMetadata();
+			if(metadata!=null){
+				if(!metadata.getFileName().equals(bgenFile.getName())){
+					throw new GenotypeDataException("Sample name between bgenix and bgen is not equal. Invalid Bgen and Bgenix combination.");
+				}
 				
+				if(metadata.getFileSize()!=bgenFile.length()){
+					throw new GenotypeDataException("Number of expected bytes is different to the numer of observed bytes. Invalid Bgen and Bgenix combination.");
+				}
+				
+				this.bgenFile.seek(0);
+				byte[] firstBytes = new byte[1000];
+				this.bgenFile.read(firstBytes, 0, 1000);
+				if(!Arrays.equals(metadata.getFirst1000bytes(), firstBytes)){
+					throw new GenotypeDataException("First 1000 bytes of meta data and actual data are not equal. Invalid Bgen and Bgenix combination.");
+				}
+			}
+		} else {
+//			throw new GenotypeDataException("Currently only bgen genotype data indexed using bgenix is supported.");
 		}
-
 		
-
+		long lastSnpStart = snpOffset + 4;
+		
+		//Read the first snp to get into genotype-io.
+		readGeneticVariant(lastSnpStart, (int) sampleCount,this.fileLayout, this.snpBlockRepresentation);
 	}
 
 	/**
-	 * Convert 4 bytes to unsigned 32 bit int from index. Returns long since java
-	 * does not have unsigned int
+	 * Convert 4 bytes to unsigned 32 bit int from index. Returns long since
+	 * java does not have unsigned int
 	 *
 	 * https://stackoverflow.com/questions/13203426/convert-4-bytes-to-an-unsigned-32-bit-integer-and-storing-it-in-a-long
 	 *
@@ -212,7 +228,7 @@ public class BgenGenotypeData {
 		value |= (bytes[3 + startIndex] << 24) & 0xFFFFFFFF;
 		return value;
 	}
-	
+
 	/**
 	 * Convert 2 bytes to unsigned 16 bit int from start index.
 	 *
@@ -226,6 +242,163 @@ public class BgenGenotypeData {
 		int value = bytes[0 + startIndex] & 0xFF;
 		value |= (bytes[1 + startIndex] << 8) & 0xFFFF;
 		return value;
-	}	
+	}
 	
+	private void readGeneticVariant(long lastSnpStart, int sampleCount, layout currentFileLayout, blockRepresentation currentBlockRepresentation) throws IOException {
+		//Binary index writer.
+//		for (int snpI = 0; snpI < variantCount; ++snpI) {
+		if(currentFileLayout.equals(layout.layOut_1)){
+			lastSnpStart +=4;
+		}
+		
+		this.bgenFile.seek(lastSnpStart);
+		//Not sure if we want to do the buffer search here. Or we might be able to take a smaller set.
+		byte[] snpInfoBuffer = new byte[8096];
+//		int snpInfoBufferSize = 
+		this.bgenFile.read(snpInfoBuffer, 0, snpInfoBuffer.length);
+		int snpInfoBufferPos = 0;
+
+//		if (snpInfoBufferSize < 20) {
+//			throw new GenotypeDataException("Error reading bgen snp data. File is corrupt");
+//		}
+
+		int fieldLength = getUInt16(snpInfoBuffer, snpInfoBufferPos);
+		LOGGER.debug("Snp ID length " + fieldLength);
+		this.bgenFile.skipBytes(fieldLength);
+
+		snpInfoBufferPos += 2 + fieldLength; // skip id length and snp id
+
+		fieldLength = getUInt16(snpInfoBuffer, snpInfoBufferPos);
+		LOGGER.debug("Snp RS length " + fieldLength);
+
+		snpInfoBufferPos += 2;
+		String snpId = new String(snpInfoBuffer, snpInfoBufferPos, fieldLength, charset);
+		System.out.println(snpId);
+
+		snpInfoBufferPos += fieldLength;
+
+		fieldLength = getUInt16(snpInfoBuffer, snpInfoBufferPos);
+		snpInfoBufferPos += 2;
+		String seqName = new String(snpInfoBuffer, snpInfoBufferPos, fieldLength, charset);
+		System.out.println(seqName);
+		snpInfoBufferPos += fieldLength;
+
+			long snpPosLong = getUInt32(snpInfoBuffer, snpInfoBufferPos);
+			snpInfoBufferPos += 4;
+			if (snpPosLong > Integer.MAX_VALUE) {
+				throw new GenotypeDataException("SNP pos larger than (2^31)-1 not supported");
+			}
+
+			int snpPos = (int) snpPosLong;
+
+			System.out.println("SNP pos " + snpPos);
+			
+			int numberOfAlleles = 2;
+			if(currentFileLayout.equals(layout.layOut_2)){
+				numberOfAlleles = getUInt16(snpInfoBuffer, snpInfoBufferPos);
+				snpInfoBufferPos += 2;
+				System.out.println("SNP Alleles " + numberOfAlleles);
+			}
+			
+			ArrayList<String> alleles = new ArrayList<>();
+			for(int i =0; i < numberOfAlleles; i++){
+				long fieldLengthLong = getUInt32(snpInfoBuffer, snpInfoBufferPos);
+				snpInfoBufferPos += 4;
+				if (fieldLengthLong > Integer.MAX_VALUE) {
+					throw new GenotypeDataException("SNP with allele longer than (2^31)-1 characters not supported");
+				}
+				String a = new String(snpInfoBuffer, snpInfoBufferPos, (int) fieldLengthLong, charset);
+				snpInfoBufferPos += ((int) fieldLengthLong);
+				alleles.add(a);
+			}
+			
+			StringBuilder alleleBuffer = new StringBuilder();
+			for(int i=0;i<alleles.size(); i++){
+				alleleBuffer.append(alleles.get(i));
+				alleleBuffer.append('/');
+			}
+			System.out.println(alleleBuffer.toString());
+			
+			System.out.println("Location where genotypes start: "+this.bgenFile.getFilePointer());
+			readGenotypesFromVariant(this.bgenFile.getFilePointer(), currentFileLayout, currentBlockRepresentation, sampleCount);
+//			//move to next SNP
+//			lastSnpStart = lastSnpStart + snpInfoBufferPos + snpBlockSize;
+//			this.bgenFile.seek(lastSnpStart);
+//
+//		}
+
+	}
+	
+	private void readGenotypesFromVariant(long filePointer, layout currentFileLayout, blockRepresentation currentBlockRepresentation, int sampleCount) throws IOException {
+		this.bgenFile.seek(filePointer);
+		//Not sure if we want to do the buffer search here. Or we might be able to take a smaller set.
+		byte[] snpInfoBuffer = new byte[8096];
+//		int snpInfoBufferSize = 
+		this.bgenFile.read(snpInfoBuffer, 0, snpInfoBuffer.length);
+		int snpInfoBufferPos = 0;
+		
+		if(currentFileLayout.equals(fileLayout.layOut_1)){
+			long snpBlockSize;
+			if (currentBlockRepresentation.equals(blockRepresentation.compression_1)) {
+				snpBlockSize = getUInt32(snpInfoBuffer, snpInfoBufferPos);
+				snpBlockSize+=4;
+			} else {
+				snpBlockSize = 6 * sampleCount;
+			}
+			System.out.println("Snp block size: "+snpBlockSize);
+			byte[] snpBlockData = new byte[6 * (int) sampleCount];
+			if(currentBlockRepresentation.equals(blockRepresentation.compression_1)){
+				gzipInflater.setInput(snpInfoBuffer, snpInfoBufferPos, (int) snpBlockSize);
+				try {
+					gzipInflater.inflate(snpBlockData);
+				} catch (DataFormatException ex) {
+					throw new GenotypeDataException("Error decompressing bgen data", ex);
+				}
+				gzipInflater.reset();
+				
+				//Here we can parse from the inflater block for all the samples.
+				System.out.println(getUInt16(snpBlockData, 0) / 32768f + " " + getUInt16(snpBlockData, 2) / 32768f + " " + getUInt16(snpBlockData, 4) / 32768f);
+			} else {
+				//Here we can directly parse from the snpInfoBuffer.
+			}
+			
+
+		} else if(currentFileLayout.equals(fileLayout.layOut_2)){
+			long snpBlockSize;
+			long snpBlockSizeDecompressed;
+			if (!currentBlockRepresentation.equals(blockRepresentation.compression_0)) {
+				snpBlockSize = getUInt32(snpInfoBuffer, snpInfoBufferPos);
+				snpInfoBufferPos += 4;
+				snpBlockSizeDecompressed = getUInt32(snpInfoBuffer, snpInfoBufferPos);
+				snpInfoBufferPos += 4;
+			} else {
+				snpBlockSize = getUInt32(snpInfoBuffer, snpInfoBufferPos);
+				snpInfoBufferPos += 4;
+				snpBlockSizeDecompressed  = snpBlockSize;
+			}
+			System.out.println("Snp block size: "+snpBlockSize);
+			System.out.println("Snp block size decompressed: "+snpBlockSizeDecompressed);
+//			byte[] snpBlockData = new byte[6 * (int) sampleCount];
+
+			if (currentBlockRepresentation.equals(blockRepresentation.compression_1)) {
+//				inflater.setInput(snpInfoBuffer, snpInfoBufferPos + 4, (int) snpBlockSize - 4);
+//				try {
+//					inflater.inflate(snpBlockData);
+//				} catch (DataFormatException ex) {
+//					throw new GenotypeDataException("Error decompressing bgen data", ex);
+//				}
+//				inflater.reset();
+//				System.out.println(getUInt16(snpBlockData, 0) / 32768f + " " + getUInt16(snpBlockData, 2) / 32768f + " " + getUInt16(snpBlockData, 4) / 32768f);
+			} else if (currentBlockRepresentation.equals(blockRepresentation.compression_1)) {
+				
+			} else {
+				//Not compressed.
+			}
+
+		}
+	}
+	
+
+	
+
 }
--- Genotype-IO/src/test/java/org/molgenis/genotype/bgen/BgenGenotypeDataTest.java
+++ Genotype-IO/src/test/java/org/molgenis/genotype/bgen/BgenGenotypeDataTest.java
@@ -4,6 +4,7 @@
  */
 package org.molgenis.genotype.bgen;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import org.molgenis.genotype.ResourceTest;
@@ -19,14 +20,32 @@ public class BgenGenotypeDataTest extends ResourceTest {
 		
 	private BgenGenotypeData genotypeData;
 
-	@BeforeClass
-	public void beforeClass() throws IOException, URISyntaxException {
-		genotypeData = new BgenGenotypeData(getTest3Bgen(), getTest3Sample());
-	}
+//	@BeforeClass
+//	public void beforeClass() throws IOException, URISyntaxException {
+//		genotypeData = new BgenGenotypeData(getTest3Bgen(), getTest3Sample());
+//	}
 	
 	@Test
 	public void testSomeMethod() {
 		// TODO review the generated test code and remove the default call to fail.
+	}
 	
+    @Test
+	public void testReader_1_1() throws URISyntaxException, IOException {
+            genotypeData = new BgenGenotypeData(getTest3Bgen(), getTest3Sample());
+	}
+	
+    @Test
+	public void testReader_1_2() throws URISyntaxException, IOException {
+            File bgenixFile = getTestResourceFile("/bgenExamples/complex.bgen");
+            genotypeData = new BgenGenotypeData(bgenixFile, null);
+			bgenixFile = getTestResourceFile("/bgenExamples/example.16bits.bgen");
+			genotypeData = new BgenGenotypeData(bgenixFile, null);
+	}
+		
+	@Test
+	public void testReader_1_3() throws URISyntaxException, IOException {
+            File bgenixFile = getTestResourceFile("/bgenExamples/example.16bits.zstd.bgen");
+            genotypeData = new BgenGenotypeData(bgenixFile, null);
 	}
 }
