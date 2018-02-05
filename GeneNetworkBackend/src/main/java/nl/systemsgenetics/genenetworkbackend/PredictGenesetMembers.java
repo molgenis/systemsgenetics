2a3069589ea6c21f7098afa4d9951754aca3a4ee
Patrick Deelen <patrickdeelen@gmail.com>
2018-02-05 17:39:55 +0100

    GN

--- GeneNetworkBackend/src/main/java/nl/systemsgenetics/genenetworkbackend/PredictGenesetMembers.java
+++ GeneNetworkBackend/src/main/java/nl/systemsgenetics/genenetworkbackend/PredictGenesetMembers.java
@@ -1,5 +1,7 @@
 package nl.systemsgenetics.genenetworkbackend;
 
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
+
 /**
  *
  * @author Patrick Deelen
@@ -10,8 +12,8 @@ public class PredictGenesetMembers {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
+
+		
         
     }
     
--- GeneNetworkBackend/src/main/java/nl/systemsgenetics/genenetworkbackend/legacy/PredictGenesetMemberBasedOnTCs.java
+++ GeneNetworkBackend/src/main/java/nl/systemsgenetics/genenetworkbackend/legacy/PredictGenesetMemberBasedOnTCs.java
@@ -16,17 +16,17 @@ public class PredictGenesetMemberBasedOnTCs {
         //Load the matrix with the eigenvectors (rows = genes, columns = components, missing values are coded as NaN, components from multiple species can be combined)
         String filenameEigenvector = eigenVectorPath;
         DoubleRandomEngine randomEngine = new DRand();
-        ExpressionDataset dataset = new ExpressionDataset(filenameEigenvector);
-        ExpressionDataset datasetTransposed = new ExpressionDataset(dataset.fileName);
-        datasetTransposed.transposeDataset();
+        ExpressionDataset eigenVectorDataset = new ExpressionDataset(filenameEigenvector);
+        ExpressionDataset eigenVectorDatasetTransposed = new ExpressionDataset(eigenVectorDataset.fileName);
+        eigenVectorDatasetTransposed.transposeDataset();
 
         //Identify strata in the dataset (mix of different species, that cause different missing values per species):
         int nrStrata = 0;
-        int[] nrTCsPerStrata = new int[dataset.nrSamples];
+        int[] nrTCsPerStrata = new int[eigenVectorDataset.nrSamples];
         nrTCsPerStrata[nrStrata]++;
-        for (int s=1; s<dataset.nrSamples; s++) {
-            for (int p=0; p<dataset.nrProbes; p++) {
-                if (Double.isNaN(dataset.rawData[p][s])!=Double.isNaN(dataset.rawData[p][s - 1])) {
+        for (int s=1; s<eigenVectorDataset.nrSamples; s++) {
+            for (int p=0; p<eigenVectorDataset.nrProbes; p++) {
+                if (Double.isNaN(eigenVectorDataset.rawData[p][s])!=Double.isNaN(eigenVectorDataset.rawData[p][s - 1])) {
                     nrStrata++;
                     break;
                 }
@@ -68,13 +68,13 @@ public class PredictGenesetMemberBasedOnTCs {
             System.out.println("Inventorizing geneset:\t" + queryGenesets[q] + "\t" + geneset);
 
             //Check whether there are at least 10 genes for each TC with data for this geneset:
-            int minNrGenesPerStrata = dataset.nrProbes;
+            int minNrGenesPerStrata = eigenVectorDataset.nrProbes;
             int[] nrGenesInGenesetPerStrata = new int[nrStrata];
             int[] nrGenesNotInGenesetPerStrata = new int[nrStrata];
             for (int s=0; s<nrStrata; s++) {
                 int nrGenesAvailable = 0;
-                for (int gene=0; gene<dataset.nrProbes; gene++) {
-                    if (!Double.isNaN(datasetTransposed.rawData[strataStartColumn[s]][gene])) {
+                for (int gene=0; gene<eigenVectorDataset.nrProbes; gene++) {
+                    if (!Double.isNaN(eigenVectorDatasetTransposed.rawData[strataStartColumn[s]][gene])) {
                         if (datasetGeneset.rawData[gene][geneset]==1) {
                             nrGenesAvailable++;
                             nrGenesInGenesetPerStrata[s]++;
@@ -90,7 +90,7 @@ public class PredictGenesetMemberBasedOnTCs {
                 System.out.println("Processing geneset:\t" + queryGenesets[q] + "\t" + geneset + "\t" + minNrGenesPerStrata);
                 System.out.println("TC\tTCIndex\tNrGenesInGeneset\tT-Statistic\tZScore\tpValue\tdf");
 
-                double[] zScoreProfile = new double[dataset.nrSamples];
+                double[] zScoreProfile = new double[eigenVectorDataset.nrSamples];
                 for (int s=0; s<nrStrata; s++) {
 
                     double[] vals1 = new double[nrGenesInGenesetPerStrata[s]];
@@ -98,13 +98,13 @@ public class PredictGenesetMemberBasedOnTCs {
                     for (int tc=0; tc<nrTCsPerStrata[s]; tc++) {
                         int vals1Itr = 0;
                         int vals2Itr = 0;
-                        for (int gene=0; gene<dataset.nrProbes; gene++) {
-                            if (!Double.isNaN(datasetTransposed.rawData[tc + strataStartColumn[s]][gene])) {
+                        for (int gene=0; gene<eigenVectorDataset.nrProbes; gene++) {
+                            if (!Double.isNaN(eigenVectorDatasetTransposed.rawData[tc + strataStartColumn[s]][gene])) {
                                 if (datasetGeneset.rawData[gene][geneset]==1) {
-                                    vals1[vals1Itr] = datasetTransposed.rawData[tc + strataStartColumn[s]][gene];
+                                    vals1[vals1Itr] = eigenVectorDatasetTransposed.rawData[tc + strataStartColumn[s]][gene];
                                     vals1Itr++;
                                 } else {
-                                    vals2[vals2Itr] = datasetTransposed.rawData[tc + strataStartColumn[s]][gene];
+                                    vals2[vals2Itr] = eigenVectorDatasetTransposed.rawData[tc + strataStartColumn[s]][gene];
                                     vals2Itr++;
                                 }
                             }
@@ -134,27 +134,27 @@ public class PredictGenesetMemberBasedOnTCs {
                         pValue*=2;
                         zScoreProfile[tc + strataStartColumn[s]] = zScore;
                        
-                        System.out.println(dataset.sampleNames[tc + strataStartColumn[s]] + "\t" + (tc + strataStartColumn[s]) + "\t" + nrGenesInGenesetPerStrata[s] + "\t" + t + "\t" + zScore + "\t" + pValue + "\t" + df);
+                        System.out.println(eigenVectorDataset.sampleNames[tc + strataStartColumn[s]] + "\t" + (tc + strataStartColumn[s]) + "\t" + nrGenesInGenesetPerStrata[s] + "\t" + t + "\t" + zScore + "\t" + pValue + "\t" + df);
                     }
                 }
 
                 System.out.println("\n\n");
                 System.out.println("ProbeIndex\tEnsemblGene\tCorrelation\tP-Value\tZ-Score\tGeneInGenesetInput");
-                double[] zScoresIndividualGenes = new double[dataset.nrProbes];
-                for (int p=0; p<dataset.nrProbes; p++) {
+                double[] zScoresIndividualGenes = new double[eigenVectorDataset.nrProbes];
+                for (int p=0; p<eigenVectorDataset.nrProbes; p++) {
 
                     double[] zScoreProfileToUse = null;
 
                     //Prevent overfitting:
                     if (datasetGeneset.rawData[p][geneset]==1) {
 
-                        zScoreProfileToUse = new double[dataset.nrSamples];
+                        zScoreProfileToUse = new double[eigenVectorDataset.nrSamples];
                         int[] nrGenesInGenesetPerStrataToUse = new int[nrStrata];
                         int[] nrGenesNotInGenesetPerStrataToUse = new int[nrStrata];
                         for (int s=0; s<nrStrata; s++) {
-                            for (int gene=0; gene<dataset.nrProbes; gene++) {
+                            for (int gene=0; gene<eigenVectorDataset.nrProbes; gene++) {
                                 if (p!=gene) {
-                                    if (!Double.isNaN(datasetTransposed.rawData[strataStartColumn[s]][gene])) {
+                                    if (!Double.isNaN(eigenVectorDatasetTransposed.rawData[strataStartColumn[s]][gene])) {
                                         if (datasetGeneset.rawData[gene][geneset]==1) {
                                             nrGenesInGenesetPerStrataToUse[s]++;
                                         } else {
@@ -170,14 +170,14 @@ public class PredictGenesetMemberBasedOnTCs {
                             for (int tc=0; tc<nrTCsPerStrata[s]; tc++) {
                                 int vals1Itr = 0;
                                 int vals2Itr = 0;
-                                for (int gene=0; gene<dataset.nrProbes; gene++) {
+                                for (int gene=0; gene<eigenVectorDataset.nrProbes; gene++) {
                                     if (p!=gene) {
-                                        if (!Double.isNaN(datasetTransposed.rawData[tc + strataStartColumn[s]][gene])) {
+                                        if (!Double.isNaN(eigenVectorDatasetTransposed.rawData[tc + strataStartColumn[s]][gene])) {
                                             if (datasetGeneset.rawData[gene][geneset]==1) {
-                                                vals1[vals1Itr] = datasetTransposed.rawData[tc + strataStartColumn[s]][gene];
+                                                vals1[vals1Itr] = eigenVectorDatasetTransposed.rawData[tc + strataStartColumn[s]][gene];
                                                 vals1Itr++;
                                             } else {
-                                                vals2[vals2Itr] = datasetTransposed.rawData[tc + strataStartColumn[s]][gene];
+                                                vals2[vals2Itr] = eigenVectorDatasetTransposed.rawData[tc + strataStartColumn[s]][gene];
                                                 vals2Itr++;
                                             }
                                         }
@@ -217,9 +217,9 @@ public class PredictGenesetMemberBasedOnTCs {
 
                     Vector vec1 = new Vector();
                     Vector vec2 = new Vector();
-                    for (int tc=0; tc<dataset.nrSamples; tc++) {
-                        if (!Double.isNaN(dataset.rawData[p][tc])) {
-                            vec1.add(dataset.rawData[p][tc]);
+                    for (int tc=0; tc<eigenVectorDataset.nrSamples; tc++) {
+                        if (!Double.isNaN(eigenVectorDataset.rawData[p][tc])) {
+                            vec1.add(eigenVectorDataset.rawData[p][tc]);
                             vec2.add(zScoreProfileToUse[tc]);
                         }
                     }
@@ -249,13 +249,13 @@ public class PredictGenesetMemberBasedOnTCs {
                     pValue*=2;
 
                     zScoresIndividualGenes[p] = zScore;
-                    System.out.println(p + "\t" + dataset.probeNames[p] + "\t" + correlation + "\t" + pValue + "\t" + zScore + "\t" + datasetGeneset.rawData[p][geneset]);
+                    System.out.println(p + "\t" + eigenVectorDataset.probeNames[p] + "\t" + correlation + "\t" + pValue + "\t" + zScore + "\t" + datasetGeneset.rawData[p][geneset]);
                 }
 
                 System.out.println("\n\n");
                 Vector vecInGeneset = new Vector();
                 Vector vecNotInGeneset = new Vector();
-                for (int p=0; p<dataset.nrProbes; p++) {
+                for (int p=0; p<eigenVectorDataset.nrProbes; p++) {
                     if (datasetGeneset.rawData[p][geneset]==1) {
                         vecInGeneset.add(zScoresIndividualGenes[p]);
                     } else {
@@ -274,9 +274,9 @@ public class PredictGenesetMemberBasedOnTCs {
                 WilcoxonMannWhitney wmw = new WilcoxonMannWhitney();
                 double pValue = wmw.returnWilcoxonMannWhitneyPValue(vals1, vals2);
                 System.out.println("PredictionPerformanceGeneset:\t" + queryGenesets[q] + "\tGenesetNr:\t" + geneset + "\t\tNrGenesInGeneset\t" + vals1.length + "\tNrGenesOutsideGeneset:\t" + vals2.length + "\tP-Value:\t" + pValue + "\tAUC:\t" + wmw.getAUC());
-
+				System.out.println();
                 //Replace the 1 or 0 values with the Z-Scores:
-                for (int p=0; p<dataset.nrProbes; p++) {
+                for (int p=0; p<eigenVectorDataset.nrProbes; p++) {
                     datasetGeneset.rawData[p][geneset] = zScoresIndividualGenes[p];
                 }
                 
