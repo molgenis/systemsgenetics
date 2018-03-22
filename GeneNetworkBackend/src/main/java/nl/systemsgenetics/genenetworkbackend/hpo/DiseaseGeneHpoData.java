142c0efe2875dd8fbed6268e21f6cb988398698b
Patrick Deelen <patrickdeelen@gmail.com>
2018-03-22 15:56:39 +0100

    GN

--- GeneNetworkBackend/src/main/java/nl/systemsgenetics/genenetworkbackend/hpo/DiseaseGeneHpoData.java
+++ GeneNetworkBackend/src/main/java/nl/systemsgenetics/genenetworkbackend/hpo/DiseaseGeneHpoData.java
@@ -15,11 +15,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
 import java.util.Set;
 
 /**
@@ -27,17 +28,17 @@ import java.util.Set;
  * @author patri
  */
 public class DiseaseGeneHpoData {
-	
+
 	private final HashMap<String, HashSet<String>> geneToHpos;
 	private final HashMap<String, HashSet<String>> diseaseToGenes;
 	private final HashMap<DiseaseGene, HashSet<String>> diseaseGeneToHpos; // disease_gene
 
-	public DiseaseGeneHpoData(final File diseaseGeneHpoFile, HashMap<String, ArrayList<String>> ncbiToEnsgMap, HashMap<String, ArrayList<String>> hgncToEnsgMap, HashSet<String> exludedHpo ) throws FileNotFoundException, IOException {
-		
+	public DiseaseGeneHpoData(final File diseaseGeneHpoFile, HashMap<String, ArrayList<String>> ncbiToEnsgMap, HashMap<String, ArrayList<String>> hgncToEnsgMap, HashSet<String> exludedHpo) throws FileNotFoundException, IOException {
+
 		geneToHpos = new HashMap<>();
 		diseaseToGenes = new HashMap<>();
 		diseaseGeneToHpos = new HashMap<>();
-		
+
 		final CSVParser hpoParser = new CSVParserBuilder().withSeparator('\t').withIgnoreQuotations(true).build();
 		final CSVReader hpoReader = new CSVReaderBuilder(new BufferedReader(new FileReader(diseaseGeneHpoFile))).withSkipLines(1).withCSVParser(hpoParser).build();
 
@@ -47,21 +48,21 @@ public class DiseaseGeneHpoData {
 			String hgcnId = nextLine[1];
 			String ncbiId = nextLine[2];
 			String hpo = nextLine[3];
-			
-			if(exludedHpo != null && exludedHpo.contains(hpo)){
+
+			if (exludedHpo != null && exludedHpo.contains(hpo)) {
 				continue;
 			}
-			
+
 			ArrayList<String> ensgIds = ncbiToEnsgMap.get(ncbiId);
 			if (ensgIds == null) {
 				ensgIds = hgncToEnsgMap.get(hgcnId);
 			}
 			if (ensgIds == null) {
 				System.err.println("Missing mapping for gene: " + ncbiId + " " + hgcnId);
-			} else if(ensgIds.size() > 1) {
-				System.err.println("Skipping becasue multiple ENSG IDs for gene: " + ncbiId + " " + hgcnId);				
-			}else {
-				
+			} else if (ensgIds.size() > 1) {
+				System.err.println("Skipping becasue multiple ENSG IDs for gene: " + ncbiId + " " + hgcnId);
+			} else {
+
 				String ensgId = ensgIds.get(0);
 
 				HashSet<String> geneHpos = geneToHpos.get(ensgId);
@@ -69,97 +70,190 @@ public class DiseaseGeneHpoData {
 					geneHpos = new HashSet<>();
 					geneToHpos.put(ensgId, geneHpos);
 				}
-				
+
 				geneHpos.add(hpo);
-								
+
 				HashSet<String> diseaseGenes = diseaseToGenes.get(disease);
-				if(diseaseGenes == null){
+				if (diseaseGenes == null) {
 					diseaseGenes = new HashSet<>();
 					diseaseToGenes.put(disease, diseaseGenes);
 				}
 				diseaseGenes.add(ensgId);
 
 				DiseaseGene diseaseGene = new DiseaseGene(disease, ensgId);
-				
+
 				HashSet<String> diseaseGeneHpos = diseaseGeneToHpos.get(diseaseGene);
-				if(diseaseGeneHpos == null){
+				if (diseaseGeneHpos == null) {
 					diseaseGeneHpos = new HashSet<>();
 					diseaseGeneToHpos.put(diseaseGene, diseaseGeneHpos);
 				}
 				diseaseGeneHpos.add(hpo);
-				
+
 			}
 
 		}
-		
+
+	}
+
+	public DiseaseGeneHpoData(HashMap<DiseaseGene, HashSet<String>> diseaseGeneToHpos) {
+
+		this.diseaseGeneToHpos = diseaseGeneToHpos;
+
+		geneToHpos = new HashMap<>();
+		diseaseToGenes = new HashMap<>();
+
+		for (Map.Entry<DiseaseGene, HashSet<String>> diseaseGeneToHposEntry : diseaseGeneToHpos.entrySet()) {
+
+			DiseaseGene diseaseGene = diseaseGeneToHposEntry.getKey();
+			HashSet<String> hpos = diseaseGeneToHposEntry.getValue();
+
+			HashSet<String> geneHpos = geneToHpos.get(diseaseGene.getGene());
+			if (geneHpos == null) {
+				geneHpos = new HashSet<>();
+				geneToHpos.put(diseaseGene.getGene(), geneHpos);
+			}
+
+			geneHpos.addAll(hpos);
+
+			HashSet<String> diseaseGenes = diseaseToGenes.get(diseaseGene.getDisease());
+			if (diseaseGenes == null) {
+				diseaseGenes = new HashSet<>();
+				diseaseToGenes.put(diseaseGene.getDisease(), diseaseGenes);
+			}
+			diseaseGenes.add(diseaseGene.getGene());
+
+		}
+
 	}
 
 	/**
 	 * Returns null if no phenotypes associated
-	 * 
+	 *
 	 * @param ensgId
-	 * @return 
+	 * @return
 	 */
-	public Set<String> getEnsgHpos(String ensgId){
-		
+	public Set<String> getEnsgHpos(String ensgId) {
+
 		HashSet<String> geneHpos = geneToHpos.get(ensgId);
-		
-		if(geneHpos == null){
+
+		if (geneHpos == null) {
 			return null;
 		} else {
 			return Collections.unmodifiableSet(geneHpos);
 		}
-		
+
 	}
-	
-	public Set<String> getDiseaseGenes(){
+
+	public Set<String> getDiseaseGenes() {
 		return Collections.unmodifiableSet(geneToHpos.keySet());
 	}
-	
-	public Set<String> getDiseases(){
+
+	public Set<String> getDiseases() {
 		return Collections.unmodifiableSet(diseaseToGenes.keySet());
 	}
-	
-	public Set<DiseaseGene> getDiseaseGeneHpos(){
+
+	public Set<DiseaseGene> getDiseaseGeneHpos() {
 		return Collections.unmodifiableSet(diseaseGeneToHpos.keySet());
 	}
-	
+
 	/**
 	 * Returns null if no disease genes are found
-	 * 
+	 *
 	 * @param disease
-	 * @return 
+	 * @return
 	 */
-	public Set<String> getGenesForDisease(String disease){
+	public Set<String> getGenesForDisease(String disease) {
 		HashSet<String> diseaseGenes = diseaseToGenes.get(disease);
-		
-		if(diseaseGenes == null){
+
+		if (diseaseGenes == null) {
 			return null;
 		} else {
 			return Collections.unmodifiableSet(diseaseGenes);
 		}
 	}
-	
+
 	/**
 	 * Returns null if no phenotypes associated
-	 * 
+	 *
 	 * @param diseaseGene disease_gene
-	 * @return 
+	 * @return
 	 */
-	public Set<String> getDiseaseEnsgHpos(DiseaseGene diseaseGene){
-		
+	public Set<String> getDiseaseEnsgHpos(DiseaseGene diseaseGene) {
+
 		HashSet<String> hpos = diseaseGeneToHpos.get(diseaseGene);
-		
-		if(hpos == null){
+
+		if (hpos == null) {
 			return null;
 		} else {
 			return Collections.unmodifiableSet(hpos);
 		}
-		
+
+	}
+
+	public DiseaseGeneHpoData getPermutation() {
+		return getPermutation(new Random(), null);
+	}
+
+	public DiseaseGeneHpoData getPermutation(long seed) {
+		return getPermutation(new Random(seed), null);
 	}
 	
-	public class DiseaseGene {
+	public DiseaseGeneHpoData getPermutation(long seed, ArrayList<String> backgroundGenes) {
+		return getPermutation(new Random(seed), backgroundGenes);
+	}
+	
+	public DiseaseGeneHpoData getPermutation(ArrayList<String> backgroundGenes) {
+		return getPermutation(new Random(), backgroundGenes);
+	}
+
+	private DiseaseGeneHpoData getPermutation(Random random, ArrayList<String> backgroundGenes) {
+
+		if(backgroundGenes == null){
+			backgroundGenes = new ArrayList(geneToHpos.keySet());
+		}
 		
+
+		HashMap<DiseaseGene, HashSet<String>> randomDiseaseGeneToHpos = new HashMap<>();
+
+		for (Map.Entry<DiseaseGene, HashSet<String>> diseaseGeneToHposEntry : this.diseaseGeneToHpos.entrySet()) {
+
+			DiseaseGene diseaseGene = diseaseGeneToHposEntry.getKey();
+			HashSet<String> hpos = diseaseGeneToHposEntry.getValue();
+
+			String disease = diseaseGene.getDisease();
+			String gene = diseaseGene.getGene();
+
+			HashSet<String> knownGenesForDisease = this.diseaseToGenes.get(disease);
+
+			String randomReplacementGene;
+			DiseaseGene randomDiseaseGene;
+			boolean hpoOverlap;
+
+			do {
+				randomReplacementGene = backgroundGenes.get(random.nextInt(backgroundGenes.size()));
+				randomDiseaseGene = new DiseaseGene(disease, randomReplacementGene);
+				HashSet<String> knownHposForRandomGene = this.geneToHpos.get(randomReplacementGene);
+				hpoOverlap = false;
+				if (knownHposForRandomGene != null) {
+					for (String hpo : hpos) {
+						if (knownHposForRandomGene.contains(hpo)) {
+							hpoOverlap = true;
+							break;
+						}
+					}
+				}
+			} while (hpoOverlap | knownGenesForDisease.contains(randomReplacementGene) | randomDiseaseGeneToHpos.containsKey(randomDiseaseGene));
+
+			randomDiseaseGeneToHpos.put(randomDiseaseGene, hpos);
+
+		}
+
+		return new DiseaseGeneHpoData(randomDiseaseGeneToHpos);
+
+	}
+
+	public class DiseaseGene {
+
 		private final String disease;
 		private final String gene;
 
@@ -209,9 +303,7 @@ public class DiseaseGeneHpoData {
 		public String toString() {
 			return disease + "_" + gene;
 		}
-		
-		
-			
+
 	}
-	
+
 }
--- GeneNetworkBackend/src/main/java/nl/systemsgenetics/genenetworkbackend/hpo/TestDiseaseGenePerformance.java
+++ GeneNetworkBackend/src/main/java/nl/systemsgenetics/genenetworkbackend/hpo/TestDiseaseGenePerformance.java
@@ -5,6 +5,7 @@
  */
 package nl.systemsgenetics.genenetworkbackend.hpo;
 
+import cern.colt.matrix.tdouble.DoubleMatrix1D;
 import cern.colt.matrix.tdouble.DoubleMatrix2D;
 import com.opencsv.CSVWriter;
 import java.io.BufferedReader;
@@ -21,6 +22,8 @@ import java.util.Set;
 import static nl.systemsgenetics.genenetworkbackend.ConvertHpoToMatrix.loadHgncToEnsgMap;
 import static nl.systemsgenetics.genenetworkbackend.ConvertHpoToMatrix.loadNcbiToEnsgMap;
 import static nl.systemsgenetics.genenetworkbackend.div.CalculateGenePredictability.loadSignificantTerms;
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
+import org.apache.commons.math3.stat.descriptive.moment.Variance;
 import org.apache.commons.math3.stat.ranking.NaNStrategy;
 import org.apache.commons.math3.stat.ranking.NaturalRanking;
 import org.apache.commons.math3.stat.ranking.TiesStrategy;
@@ -48,18 +51,49 @@ public class TestDiseaseGenePerformance {
 		final File hpoOboFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\HPO\\135\\hp.obo");
 		final File hpoPredictionInfoFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\Data31995Genes05-12-2017\\PCA_01_02_2018\\predictions\\hpo_predictions_auc_bonferroni.txt");
 		final File hposToExcludeFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\hpoToExclude.txt");
-		final File outputFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\hpoDiseaseBenchmark.txt");
+		final File skewnessFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\Data31995Genes05-12-2017\\PCA_01_02_2018\\predictions\\skewnessSummary.txt");
+		final boolean randomize = true;
+		final File annotationMatrixFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\Data31995Genes05-12-2017\\PCA_01_02_2018\\PathwayMatrix\\ALL_SOURCES_ALL_FREQUENCIES_phenotype_to_genes.txt_matrix.txt.gz");
+		final File backgroundForRandomize = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\Data31995Genes05-12-2017\\PCA_01_02_2018\\PathwayMatrix\\Ensembl2Reactome_All_Levels.txt_genesInPathways.txt");
+		final boolean randomizeCustomBackground = true;
+
+		final File outputFile;
+		final ArrayList<String> backgroundGenes;
+		if (randomize) {
+
+			if (randomizeCustomBackground) {
+				backgroundGenes = loadBackgroundGenes(backgroundForRandomize);
+				outputFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\hpoDiseaseBenchmarkRandomizedCustomBackground.txt");
+			} else {
+				backgroundGenes = null;
+				outputFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\hpoDiseaseBenchmarkRandomized.txt");
+			}
+
+		} else {
+			backgroundGenes = null;
+			outputFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\hpoDiseaseBenchmark.txt");
+		}
 
 		final HashMap<String, ArrayList<String>> ncbiToEnsgMap = loadNcbiToEnsgMap(ncbiToEnsgMapFile);
 		final HashMap<String, ArrayList<String>> hgncToEnsgMap = loadHgncToEnsgMap(hgncToEnsgMapFile);
 		final HashSet<String> exludedHpo = loadHpoExclude(hposToExcludeFile);
 
-		final DiseaseGeneHpoData diseaseGeneHpoData = new DiseaseGeneHpoData(diseaseGeneHpoFile, ncbiToEnsgMap, hgncToEnsgMap, exludedHpo);
+		DiseaseGeneHpoData diseaseGeneHpoData = new DiseaseGeneHpoData(diseaseGeneHpoFile, ncbiToEnsgMap, hgncToEnsgMap, exludedHpo);
+		if (randomize) {
+			diseaseGeneHpoData = diseaseGeneHpoData.getPermutation(1, backgroundGenes);
+		}
+
+		final SkewnessInfo skewnessInfo = new SkewnessInfo(skewnessFile);
 
 		LinkedHashSet<String> significantTerms = loadSignificantTerms(significantTermsFile);
 
 		DoubleMatrixDataset<String, String> predictionMatrix = DoubleMatrixDataset.loadDoubleData(predictionMatrixFile.getAbsolutePath());
 		DoubleMatrixDataset<String, String> predictionMatrixSignificant = predictionMatrix.viewColSelection(significantTerms);
+
+		DoubleMatrixDataset<String, String> annotationnMatrix = DoubleMatrixDataset.loadDoubleData(annotationMatrixFile.getAbsolutePath());
+		DoubleMatrixDataset<String, String> annotationMatrixSignificant = annotationnMatrix.viewColSelection(significantTerms);
+
+		HashMap<String, MeanSd> hpoMeanSds = calculatePathayMeansOfAnnotatedGenes(predictionMatrixSignificant, annotationMatrixSignificant);
 
 		Map<String, PredictionInfo> predictionInfo = HpoFinder.loadPredictionInfo(hpoPredictionInfoFile);
 
@@ -73,20 +107,26 @@ public class TestDiseaseGenePerformance {
 
 		CSVWriter writer = new CSVWriter(new FileWriter(outputFile), '\t', '\0', '\0', "\n");
 
-		String[] outputLine = new String[5];
+		String[] outputLine = new String[11];
 		int c = 0;
 		outputLine[c++] = "Disease";
 		outputLine[c++] = "Gene";
 		outputLine[c++] = "Rank";
+		outputLine[c++] = "Z-score";
+		outputLine[c++] = "HPO_skewness";
+		outputLine[c++] = "Other_mean_skewness";
+		outputLine[c++] = "Other_max_skewness";
+		outputLine[c++] = "HPO_phenotypic_match_score";
 		outputLine[c++] = "HPO_count";
 		outputLine[c++] = "HPO_terms";
+		outputLine[c++] = "HPO_terms_match_score";
 		writer.writeNext(outputLine);
 
-		for ( DiseaseGeneHpoData.DiseaseGene diseaseGene : diseaseGeneHpoData.getDiseaseGeneHpos()) {
+		for (DiseaseGeneHpoData.DiseaseGene diseaseGene : diseaseGeneHpoData.getDiseaseGeneHpos()) {
 
 			String gene = diseaseGene.getGene();
 			String disease = diseaseGene.getDisease();
-			
+
 			if (!predictionMatrixSignificant.containsRow(gene)) {
 				continue;
 			}
@@ -119,18 +159,60 @@ public class TestDiseaseGenePerformance {
 
 			int diseaseGeneIndex = predictionMatrixSignificant.getRowIndex(gene);
 
+			double zscore = geneScores[diseaseGeneIndex];
 			double rank = (totalGenes - geneRanks[diseaseGeneIndex]) + 1;
+
+			double hpoPhenotypicMatchScore = 0;
+			StringBuilder individualMatchScore = new StringBuilder();
+			boolean notFirst = false;
+			int usedHpos = 0;
+
+			for (String hpo : geneHpos) {
+
+				if (!predictionMatrixSignificant.containsCol(hpo)) {
+					continue;
+				}
+
+				usedHpos++;
+
+				MeanSd hpoMeanSd = hpoMeanSds.get(hpo);
+
+				double hpoPredictionZ = predictionMatrixSignificant.getElement(gene, hpo);
+
+				double hpoPredictionOutlierScore = ((hpoPredictionZ - hpoMeanSd.getMean()) / hpoMeanSd.getSd());
+
+				if (notFirst) {
+					notFirst = true;
+					individualMatchScore.append(';');
+				}
+				individualMatchScore.append(hpoPredictionOutlierScore);
+
+				hpoPhenotypicMatchScore += hpoPredictionOutlierScore;
+
+			}
+
+			if (usedHpos == 0) {
+				hpoPhenotypicMatchScore = Double.NaN;
+			} else {
+				hpoPhenotypicMatchScore = hpoPhenotypicMatchScore / usedHpos;
+			}
 
 			c = 0;
 			outputLine[c++] = disease;
 			outputLine[c++] = gene;
 			outputLine[c++] = String.valueOf(rank);
+			outputLine[c++] = String.valueOf(zscore);
+			outputLine[c++] = String.valueOf(skewnessInfo.getHpoSkewness(gene));
+			outputLine[c++] = String.valueOf(skewnessInfo.getMeanSkewnessExHpo(gene));
+			outputLine[c++] = String.valueOf(skewnessInfo.getMaxSkewnessExHpo(gene));
+			outputLine[c++] = String.valueOf(hpoPhenotypicMatchScore);
 			outputLine[c++] = String.valueOf(geneHpos.size());
 			outputLine[c++] = String.join(";", geneHpos);
+			outputLine[c++] = individualMatchScore.toString();
 			writer.writeNext(outputLine);
 
 		}
-		
+
 		writer.close();
 
 	}
@@ -147,6 +229,73 @@ public class TestDiseaseGenePerformance {
 		}
 
 		return hpos;
+
+	}
+
+	private static HashMap<String, MeanSd> calculatePathayMeansOfAnnotatedGenes(DoubleMatrixDataset<String, String> predictionMatrixSignificant, DoubleMatrixDataset<String, String> annotationMatrixSignificant) {
+
+		HashMap<String, MeanSd> pathwayMeanSdMap = new HashMap<>(predictionMatrixSignificant.columns());
+
+		Mean meanCalculator = new Mean();
+		Variance varianceCalculator = new Variance();
+
+		for (String pathway : predictionMatrixSignificant.getColObjects()) {
+
+			meanCalculator.clear();
+			varianceCalculator.clear();
+
+			DoubleMatrix1D pathwayPredictions = predictionMatrixSignificant.getCol(pathway);
+			DoubleMatrix1D pathwayAnnotations = annotationMatrixSignificant.getCol(pathway);
+
+			for (int g = 0; g < pathwayPredictions.size(); ++g) {
+				if (pathwayAnnotations.get(g) != 0) {
+					meanCalculator.increment(pathwayPredictions.getQuick(g));
+					varianceCalculator.increment(pathwayPredictions.getQuick(g));
+				}
+			}
+
+			double v = varianceCalculator.getResult();
+
+			pathwayMeanSdMap.put(pathway, new MeanSd(meanCalculator.getResult(), v * v));
+
+		}
+
+		return pathwayMeanSdMap;
+
+	}
+
+	private static class MeanSd {
+
+		private final double mean;
+		private final double sd;
+
+		public MeanSd(double mean, double sd) {
+			this.mean = mean;
+			this.sd = sd;
+		}
+
+		public double getMean() {
+			return mean;
+		}
+
+		public double getSd() {
+			return sd;
+		}
+
+	}
+
+	public static ArrayList<String> loadBackgroundGenes(File backgroundForRandomize) throws IOException {
+
+		ArrayList<String> genes = new ArrayList<>();
+
+		BufferedReader reader = new BufferedReader(new FileReader(backgroundForRandomize));
+
+		String line;
+		while ((line = reader.readLine()) != null) {
+			genes.add(line);
+		}
+
+		return genes;
 
 	}
 
