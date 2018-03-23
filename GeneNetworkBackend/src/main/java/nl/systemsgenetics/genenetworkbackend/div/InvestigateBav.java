6b5918fb157698c6174b6a8cf2ddbc7ea7d53656
Patrick Deelen <patrickdeelen@gmail.com>
2018-03-23 15:59:25 +0100

    GN

--- GeneNetworkBackend/src/main/java/nl/systemsgenetics/genenetworkbackend/div/InvestigateBav.java
+++ GeneNetworkBackend/src/main/java/nl/systemsgenetics/genenetworkbackend/div/InvestigateBav.java
@@ -9,11 +9,16 @@ import com.opencsv.CSVParser;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import gnu.trove.map.hash.TObjectDoubleHashMap;
+import gnu.trove.map.hash.TObjectIntHashMap;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import umcg.genetica.math.matrix2.DoubleMatrixDataset;
 
 /**
  *
@@ -28,26 +33,81 @@ public class InvestigateBav {
 
 		final File rankFolder = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\BenchmarkSamples\\Prioritisations\\rankingCandidateGenes");
 		final File samplesFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\BenchmarkSamples\\BavSamples.txt");
+		final File mutationMatrixFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\BenchmarkSamples\\MutationMatrix.txt");
 
 		ArrayList<String> samples = loadLines(samplesFile);
-		
+
 		final CSVParser parser = new CSVParserBuilder().withSeparator('\t').withIgnoreQuotations(true).build();
-		
-		
-		for(String sample : samples){
-			
+
+		TObjectIntHashMap<String> geneCount = new TObjectIntHashMap();
+		TObjectDoubleHashMap<String> geneZscore = new TObjectDoubleHashMap<>();
+		HashMap<String, String> geneOtherHpo = new HashMap<>();
+		HashMap<String, HashSet<String>> geneMutatedSamples = new HashMap<>();
+
+		for (String sample : samples) {
+
 			File sampleFile = new File(rankFolder, sample + ".txt");
 			final CSVReader reader = new CSVReaderBuilder(new BufferedReader(new FileReader(sampleFile))).withSkipLines(1).withCSVParser(parser).build();
-			
-			
-			
-			
+
+			for (int i = 0; i < 20; i++) {
+
+				String[] line = reader.readNext();
+				String gene = line[1];
+				double zscore = Double.parseDouble(line[3]);
+				String otherHpos = line[5];
+
+				geneZscore.put(gene, zscore);
+				geneCount.adjustOrPutValue(gene, 1, 1);
+				geneOtherHpo.put(gene, otherHpos);
+
+				geneMutatedSamples.putIfAbsent(gene, new HashSet<>());
+				geneMutatedSamples.get(gene).add(sample);
+
+			}
+
+			reader.close();
+
 		}
-		
-		
+
+		ArrayList<String> recurrentGenes = new ArrayList<>();
+		for (String gene : geneCount.keySet()) {
+			int count = geneCount.get(gene);
+
+			if (count > 1) {
+
+				if (geneZscore.get(gene) > 2) {
+					recurrentGenes.add(gene);
+				}
+
+				System.out.println(gene + "\t" + count + "\t" + geneZscore.get(gene) + "\t" + geneOtherHpo.get(gene) + "\t" + String.join(";", geneMutatedSamples.get(gene)));
+			}
+		}
+
+		DoubleMatrixDataset<String, String> mutationMatrix = new DoubleMatrixDataset<>(recurrentGenes, samples);
+
+		for (String sample : samples) {
+
+			File sampleFile = new File(rankFolder, sample + ".txt");
+			final CSVReader reader = new CSVReaderBuilder(new BufferedReader(new FileReader(sampleFile))).withSkipLines(1).withCSVParser(parser).build();
+
+			String[] nextLine;
+			while ((nextLine = reader.readNext()) != null) {
+				String gene = nextLine[1];
+
+				if (mutationMatrix.containsRow(gene)) {
+
+					mutationMatrix.setElement(gene, sample, 1);
+
+				}
+
+			}
+
+		}
+
+		mutationMatrix.save(mutationMatrixFile);
+
 	}
-	
-	
+
 	public static ArrayList<String> loadLines(File file) throws IOException {
 
 		ArrayList<String> lines = new ArrayList<>();
