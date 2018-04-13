e7645cf639aba6b2742a6d0dc6185009b49f2254
npklein <niekdeklein@gmail.com>
2018-04-13 12:58:28 +0200

    move anova to Statistics

--- Decon2/Decon-eQTL/src/decon_eQTL/Statistics.java
+++ Decon2/Decon-eQTL/src/decon_eQTL/Statistics.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import org.apache.commons.math3.distribution.NormalDistribution;
 
+import JSci.maths.statistics.FDistribution;
+
 import java.lang.Math;
 
 /**
@@ -256,6 +258,76 @@ public class Statistics
 	public static double AIC(double[] residuals, int npar){
 		int k=2;
 		return -2*logLik(residuals) + k*npar;
+	}
+	
+	/**
+	 * Compare and return the p-value of two linear models being
+	 * significantly different
+	 *
+	 * From Joris Meys: http://stackoverflow.com/a/35458157/651779 1.
+	 * calculate MSE for the largest model by dividing the Residual Sum of
+	 * Squares (RSS) by the degrees of freedom (df) 2. calculate the
+	 * MSEdifference by substracting the RSS of both models (result is
+	 * "Sum of Sq." in the R table), substracting the df for both models
+	 * (result is "Df" in the R table), and divide these numbers. 3. Divide
+	 * 2 by 1 and you have the F value 4. calculate the p-value using the F
+	 * value in 3 and for df the df-difference in the numerator and df of
+	 * the largest model in the denominator. For more info:
+	 * http://www.bodowinter.com/tutorial/bw_anova_general.pdf
+	 * 
+	 * @param sumOfSquaresModelA A vector with the genotype of all samples
+	 * for *one* eQTL-gene pair
+	 * 
+	 * @param sumOfSquaresModelB A vector with the expression levels of all
+	 * samples for *one* eQTL-gene pair
+	 * 
+	 * @param degreesOfFreedomA A 2D list with for all samples the different
+	 * cell counts
+	 * 
+	 * @param degreesOfFreedomB A 2D list with for all samples the different
+	 * cell counts
+	 * 
+	 * @param no_intercept	If intercept was removed to calculate the sum of squares
+	 * 
+	 * @return The p-value result from comparing two linear models with the
+	 * the Anova test
+	 */
+	public static double anova(double sumOfSquaresModelA, double sumOfSquaresModelB, int degreesOfFreedomA,
+			int degreesOfFreedomB, Boolean no_intercept) {
+		if (no_intercept) {
+			// removing the intercept will give another degree of freedom
+			++degreesOfFreedomA;
+			++degreesOfFreedomB;
+		}
+		// Within-group Variance
+		double meanSquareError = sumOfSquaresModelA / degreesOfFreedomA;
+
+		int degreesOfFreedomDifference = Math.abs(degreesOfFreedomB - degreesOfFreedomA);
+		// Between-group Variance
+		// 234111286.801326
+		double meanSquareErrorDiff = Math.abs((sumOfSquaresModelA - sumOfSquaresModelB) / (degreesOfFreedomDifference));
+
+		/**
+		 * F = Between-group Variance / Within-group Variance <- high value if
+		 * variance between the models is high, and variance within the models
+		 * is low
+		 **/
+		if(meanSquareError == 0){
+			throw new RuntimeException("meanSquareError should not be 0");
+		}
+		double Fval = meanSquareErrorDiff / meanSquareError;
+		/***
+		 * Make an F distribution with degrees of freedom as parameter. If full
+		 * model and ctModel have the same number of samples, difference in df
+		 * is 1 and degreesOfFreedomB are all the terms of the ctModel (so neut%
+		 * + eos% + ... + neut% * GT + eos% * GT With 4 cell types and 1891
+		 * samples the dfs are 1883 and 1884, giving the below distribution
+		 * http://keisan.casio.com/exec/system/1180573186
+		 **/
+		FDistribution Fdist = new FDistribution(degreesOfFreedomDifference, degreesOfFreedomB);
+		/*** Calculate 1 - the probability of observing a lower Fvalue **/
+		double pval = 1 - Fdist.cumulative(Fval);
+		return pval;
 	}
 }
 
