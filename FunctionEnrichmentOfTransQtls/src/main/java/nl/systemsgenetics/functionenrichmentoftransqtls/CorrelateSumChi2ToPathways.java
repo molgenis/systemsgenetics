bb17af23bd411f3f143165a8fa139d94bf714248
Patrick Deelen <patrickdeelen@gmail.com>
2018-06-01 15:41:12 +0200

    new project

---
+++ FunctionEnrichmentOfTransQtls/src/main/java/nl/systemsgenetics/functionenrichmentoftransqtls/CorrelateSumChi2ToPathways.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.systemsgenetics.functionenrichmentoftransqtls;
+
+/**
+ *
+ * @author patri
+ */
+public class CorrelateSumChi2ToPathways {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		
+		//String pathwayMatrixFile = 
+		
+		
+	}
+	
+}
