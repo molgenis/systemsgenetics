9e887a5cf318eb563041c133cb6da53adcbd8ed8
Patrick Deelen <patrickdeelen@gmail.com>
2018-06-01 15:49:17 +0200

    x

--- FunctionEnrichmentOfTransQtls/src/main/java/nl/systemsgenetics/functionenrichmentoftransqtls/CorrelateSumChi2ToPathways.java
+++ FunctionEnrichmentOfTransQtls/src/main/java/nl/systemsgenetics/functionenrichmentoftransqtls/CorrelateSumChi2ToPathways.java
@@ -5,6 +5,10 @@
  */
 package nl.systemsgenetics.functionenrichmentoftransqtls;
 
+import java.io.File;
+import java.io.IOException;
+import umcg.genetica.math.matrix2.DoubleMatrixDataset;
+
 /**
  *
  * @author patri
@@ -14,11 +18,16 @@ public class CorrelateSumChi2ToPathways {
 	/**
 	 * @param args the command line arguments
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		final File pathwayMatrixFile = new File("C:\\UMCG\\Genetica\\Projects\\GeneNetwork\\Data31995Genes05-12-2017\\PCA_01_02_2018\\PathwayMatrix\\ALL_SOURCES_ALL_FREQUENCIES_phenotype_to_genes.txt_matrix.txt.gz");
+		final File sumChi2MatrixFile = new File("C:\\UMCG\\Genetica\\Projects\\eQtlMeta\\");
+
+		DoubleMatrixDataset pathwayMatrix = DoubleMatrixDataset.loadDoubleData(pathwayMatrixFile.getAbsolutePath());
+		DoubleMatrixDataset sumChi2Matrix = DoubleMatrixDataset.loadDoubleData(sumChi2MatrixFile.getAbsolutePath());
 		
-		//String pathwayMatrixFile = 
 		
-		
+
 	}
-	
+
 }
