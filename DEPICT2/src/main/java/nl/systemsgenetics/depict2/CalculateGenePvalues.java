a42a10a825c7bf070f371c77ad5b8bb104852063
Patrick Deelen <patrickdeelen@gmail.com>
2019-01-29 19:43:29 +0100

    DEPICT2

---
+++ DEPICT2/src/main/java/nl/systemsgenetics/depict2/CalculateGenePvalues.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.systemsgenetics.depict2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.molgenis.genotype.RandomAccessGenotypeData;
+import umcg.genetica.math.matrix2.DoubleMatrixDataset;
+
+/**
+ *
+ * @author patri
+ */
+public class CalculateGenePvalues {
+
+	/**
+	 * 
+	 * @param variantPhenotypeMatrix rows: variants, cols: phenotypes
+	 * @param referenceGenotypes genotype data used to calculate LD between SNPs
+	 * @param genes genes for which to calculate p-values. 
+	 * @param snpMapping genomic positions variants 
+	 * @return gene p-value matrix for each phenotype. rows: genes in same order as genes parameter, cols: phenotypes
+	 */
+	public static DoubleMatrixDataset<String, String> calculatorGenePvalues(DoubleMatrixDataset<String, String> variantPhenotypeMatrix, RandomAccessGenotypeData referenceGenotypes, ArrayList<Genes> genes, HashMap<String, ChrPos> snpMapping){
+		
+		
+		
+	}
+	
+}
