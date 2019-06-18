3c3d541e782abff80636eebcfca345154afe5745
Patrick Deelen <patrickdeelen@gmail.com>
2019-06-18 16:48:36 +0200

    Update Depict2.java

--- DEPICT2/src/main/java/nl/systemsgenetics/depict2/Depict2.java
+++ DEPICT2/src/main/java/nl/systemsgenetics/depict2/Depict2.java
@@ -378,7 +378,7 @@ public class Depict2 {
 
 		final List<PathwayDatabase> pathwayDatabases = options.getPathwayDatabases();
 
-		HashMap<PathwayDatabase, DoubleMatrixDataset<String, String>> enrichments = PathwayEnrichments.performEnrichmentAnalysis(genePvalues, genePvaluesNullGwas, selectedGenes, pathwayDatabases, genes, options.getOutputBasePath(), null, options.getPermutationGeneCorrelations(), options.getPermutationPathwayEnrichment(), options.getGenePruningR(), options.isIgnoreGeneCorrelations());
+		HashMap<PathwayDatabase, DoubleMatrixDataset<String, String>> enrichments = PathwayEnrichments.performEnrichmentAnalysis(genePvalues, genePvaluesNullGwas, selectedGenes, pathwayDatabases, genes, options.getOutputBasePath(), null, options.getPermutationGeneCorrelations(), options.getPermutationPathwayEnrichment(), options.getGenePruningR(), options.isIgnoreGeneCorrelations(), options.isForceNormalGenePvalues(), options.isForceNormalPathwayPvalues());
 
 		PathwayEnrichments.saveEnrichmentsToExcel(pathwayDatabases, options.getOutputBasePath(), enrichments, genePvalues.getColObjects(), false);
 
