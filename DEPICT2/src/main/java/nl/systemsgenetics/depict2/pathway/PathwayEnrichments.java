ae025876dce7549055aa0223578a4cfe050616f5
Patrick Deelen <patrickdeelen@gmail.com>
2020-06-18 12:21:48 +0200

    Revert "Revert "DS""
    This reverts commit 1376bad0dd37063f4fccd2b960edeb391dbe4119.

--- DEPICT2/src/main/java/nl/systemsgenetics/depict2/pathway/PathwayEnrichments.java
+++ DEPICT2/src/main/java/nl/systemsgenetics/depict2/pathway/PathwayEnrichments.java
@@ -22,14 +22,12 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.stream.IntStream;
 
 import me.tongfei.progressbar.ProgressBar;
@@ -323,11 +321,13 @@ public class PathwayEnrichments {
 						geneZscoresNullGwasSubsetGeneCorrelations = createLocalGeneCorrelation(geneZscoresNullGwasCorrelationSubset, armGenes, geneCorrelationWindow);
 					}
 					// geneZscoresNullGwasSubsetGeneCorrelations = geneZscoresNullGwasCorrelationSubset.viewDice().calculateCorrelationMatrix();
-
+					DenseDoubleAlgebra alg = new cern.colt.matrix.tdouble.algo.DenseDoubleAlgebra();
+					LOGGER.info("Determinant: " + alg.det(geneZscoresNullGwasSubsetGeneCorrelations.getMatrix()));
+					
 					geneZscoresNullGwasSubsetGeneCorrelations.getMatrix().assign(new DoubleFunction() {
 						@Override
 						public double apply(double argument) {
-							if(Math.abs(argument) <= 0.00001){
+							if(Math.abs(argument) <= 0.1){
 								return 0;
 							} else {
 								return argument;
@@ -335,6 +335,9 @@ public class PathwayEnrichments {
 						}
 					}
 					);
+					
+					LOGGER.info("Determinant after fix : " + alg.det(geneZscoresNullGwasSubsetGeneCorrelations.getMatrix()));
+					
 					
 					if (LOGGER.isDebugEnabled()) {
 						geneZscoresNullGwasSubsetGeneCorrelations.save(new File(debugFolder, pathwayDatabase.getName() + "_" + chrArm + "_Enrichment_geneCor.txt"));
@@ -347,7 +350,7 @@ public class PathwayEnrichments {
 					try {
 						if (this.ignoreGeneCorrelations) {
 							// Identity matrix, i.e. OLS
-							LOGGER.info("Ignoring geOLSne correlations");
+							LOGGER.info("Ignoring gene correlations and performing OLS");
 							geneInvCorMatrixSubsetMatrix = DoubleFactory2D.dense.identity(geneZscoresNullGwasSubsetGeneCorrelations.rows());
 						} else {
 							LOGGER.debug("Calculating correlation inverse");
